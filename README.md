5ddf165a314851300d2a9ec4e1b96199a0a06f32
Marcin Rataj <lidel@lidel.org>
2015-03-22 23:26:35 +0100

    Initial version with basic toggle functionality
    - Enabled by default
    - Hardcoded redirect to 127.0.0.1:8080
    - Can be disabled temporarily by clicking on the addon icon

--- README.md
+++ README.md
@@ -5,10 +5,16 @@
 Simple Firefox addon that enables access to IPFS resources via local HTTP gateway
 
 Right now it just replaces requests to `http://gateway.ipfs.io/(.*)` with `http://127.0.0.1:8080/$1`.
+Clicking on the addon icon toggles redirection.
 
 If you are using Chrome or Chromium, check [ipfs-chrome-extension](https://github.com/dylanPowers/ipfs-chrome-extension) instead.
 
 ### TODO
 
 - add option to use custom host/port instead of default `127.0.0.1:8080`
-- add support for `ipfs://` protocol and other common notations
+- add support for `ipfs://` protocol and other common notations (starting with ones proposed by ipfs-chrome-extension)
+
+
+### LICENSE
+
+Addon is released under [CC0](LICENSE). IPFS logo belongs to the http://ipfs.io project.
---
+++ index.js
@@ -0,0 +1,76 @@
+var self = require('sdk/self');
+var { Cc, Ci } = require("chrome");
+var { ToggleButton } = require("sdk/ui/button/toggle");
+var events = require("sdk/system/events");
+var iOService = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
+var version = require("./package.json").version;
+
+const enabledState = {
+  icon: {
+    "16": "./icon-on-16.png",
+    "32": "./icon-on-32.png",
+    "64": "./icon-on-64.png"
+  },
+  badge: "ON",
+  badgeColor: "#4A9EA1"
+}
+
+const disabledState = {
+  icon: {
+    "16": "./icon-off-16.png",
+    "32": "./icon-off-32.png",
+    "64": "./icon-off-64.png"
+  },
+  badge: "OFF",
+  badgeColor: "#8C8C8C"
+}
+
+var button = ToggleButton({
+  id: "ipfs-gateway-status",
+  label: "Toggle IPFS Gateway Redirect Status",
+  icon: {
+    "16": "./icon-on-16.png",
+    "32": "./icon-on-32.png",
+    "64": "./icon-on-64.png"
+  },
+  checked: true,
+  onChange: function(state) {
+    // we want a global flag
+    this.state('window', null);
+    this.checked = !this.checked;
+
+    // update GUI to reflect toggled state
+    if (state.checked) {
+      button.state(button, enabledState);
+    } else {
+      button.state(button, disabledState);
+    }
+  }
+
+});
+// enabled by default
+button.state(button, enabledState);
+
+function listener(event) {
+  var channel = event.subject.QueryInterface(Ci.nsIHttpChannel);
+  if (channel.URI.host == "gateway.ipfs.io") {
+    if (button.checked) {
+      var requestURL = channel.URI.spec;
+      var localGatewayURL = requestURL.replace(/gateway.ipfs.io/i, '127.0.0.1:8080');
+      channel.redirectTo(iOService.newURI(localGatewayURL, null, null));
+    }
+
+    // some free metrics
+    channel.setRequestHeader("x-ipfs-firefox-addon-version", version, false);
+    channel.setRequestHeader("x-ipfs-firefox-addon-state", button.checked, false);
+  }
+}
+events.on("http-on-modify-request", listener);
+
+// TODO: when we have more functionality, we should add tests:
+// a dummy function, to show how tests work.
+// to see how to test this function, look at ../test/test-main.js
+function dummy(text, callback) {
+  callback(text);
+}
+exports.dummy = dummy;
