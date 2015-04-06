5314fb2eaa63c2eda3c1557bb3a1b134273f5a56
Marcin Rataj <lidel@lidel.org>
2015-04-06 20:49:57 +0200

    Added test-gateways.js with actual tests for HTTP redirect (#9)

---
+++ test/test-gateways.js
@@ -0,0 +1,43 @@
+var gw = require('./gateways.js');
+var gui = require('./gui.js');
+const tabs = require('sdk/tabs');
+const path = 'ipfs/QmTkzDwWqPbnAh5YiV5VwcTLnGdwSNsNTn2aDxdXBFca7D/example#/ipfs/QmSsNVuALPa1TW1GDahup8fFDqo95iFyPE7E6HpqDivw3p/readme.md';
+const button = gui.toggleButton;
+
+let {
+  before
+} = require('sdk/test/utils');
+before(exports, function(name, assert) { // jshint unused:false
+  // reset redirect state before each test
+  gw.enableHttpGatewayRedirect(button);
+});
+
+exports['test enabled redirect'] = function(assert, done) {
+  // open Public Gateway URL and check if it gets redirected to gw.customUri()
+  gw.enableHttpGatewayRedirect(button);
+  tabs.open({
+    url: gw.publicUri().spec + path,
+    onReady: function onReady(tab) {
+      assert.equal(tab.url, gw.customUri().spec + path, 'expected redirect');
+      tab.close(done);
+    }
+  });
+
+};
+
+
+exports['test disabled redirect'] = function(assert, done) {
+  // same check, but with disabled redirection
+  gw.enableHttpGatewayRedirect(button);
+  gw.disableHttpGatewayRedirect(button);
+  tabs.open({
+    url: gw.publicUri().spec + path,
+    onReady: function onReady(tab) {
+      assert.equal(tab.url, gw.publicUri().spec + path, 'no redirect expected');
+      gw.enableHttpGatewayRedirect(button);
+      tab.close(done);
+    }
+  });
+};
+
+require('sdk/test').run(exports);
--- test/test-gui.js
+++ test/test-gui.js
@@ -1,17 +1,29 @@
 var gui = require('./gui.js');
 var gw = require('./gateways.js');
 var prefs = require('sdk/simple-prefs').prefs;
+const button = gui.toggleButton;
 
-exports['test toggleButton'] = function(assert) {
-  var button = gui.toggleButton;
-  assert.equal(button.id, 'ipfs-gateway-status', 'button id');
-  assert.equal(button.checked, prefs.useCustomGateway, 'state equal prefs.useCustomGateway');
-
+let {
+  before
+} = require('sdk/test/utils');
+before(exports, function(name, assert) { // jshint unused:false
+  // reset redirect state before each test
   gw.enableHttpGatewayRedirect(button);
-  assert.equal(button.checked, prefs.useCustomGateway, 'state after gw.enableHttpGatewayRedirect()');
+});
 
+exports['test toggleButton attributes'] = function(assert) {
+  assert.equal(button.id, 'ipfs-gateway-status', 'button id');
+  assert.equal(button.checked, prefs.useCustomGateway, 'default state equal prefs.useCustomGateway');
+};
+
+exports['test disabled toggleButton'] = function(assert) {
   gw.disableHttpGatewayRedirect(button);
   assert.equal(button.checked, prefs.useCustomGateway, 'state after gw.disableHttpGatewayRedirect()');
+};
+
+exports['test enabled toggleButton'] = function(assert) {
+  gw.enableHttpGatewayRedirect(button);
+  assert.equal(button.checked, prefs.useCustomGateway, 'state after gw.enableHttpGatewayRedirect()');
 };
 
 require('sdk/test').run(exports);
