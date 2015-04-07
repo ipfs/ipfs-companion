299e67627ab406611e1037da96c473a548585ace
Marcin Rataj <lidel@lidel.org>
2015-04-08 00:10:38 +0200

    More Sleep. Less Bugs. One day.

--- test/cfx_test_workaround_for_1103385.sh
+++ test/cfx_test_workaround_for_1103385.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 CFX_OUT=$(cfx test --verbose 2>&1 | tee >(cat - >&2)  ; (exit ${PIPESTATUS[0]}))
 RESULT=$?
-TOTAL=$(echo $CFX_OUT | sed -n 's/.*\([0-9]\+\) tests passed.*/\1/p')
-PASSED=$(echo $CFX_OUT | sed -n 's/.*\([0-9]\+\) of [0-9]\+ tests passed.*/\1/p')
+TOTAL=$(echo $CFX_OUT | sed -n 's/.* \([0-9]\+\) tests passed.*/\1/p')
+PASSED=$(echo $CFX_OUT | sed -n 's/.* \([0-9]\+\) of [0-9]\+ tests passed.*/\1/p')
 BUG=$(echo "$CFX_OUT" | grep "Plural form unknown for locale \"null\"")
 TESTS_FAILED=$(echo $CFX_OUT | sed -n 's/.*The following tests failed:\(.*\)Total time:.*/\1/p' | tr -d '[[:space:]]')
 
