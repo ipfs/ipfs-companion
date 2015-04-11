6d21d897a3d60fb549ebe9944c347c2bd8a81d8d
Marcin Rataj <lidel@lidel.org>
2015-04-11 20:33:32 +0200

    Support ipfs:// and ipns:// (in addition to ones without two slashes)

--- test/test-protocols.js
+++ test/test-protocols.js
@@ -1,31 +1,36 @@
 var proto = require('./protocols.js');
 var gw = require('./gateways.js');
 const pubGwUri = gw.publicUri();
+const ipfsPath = 'QmTkzDwWqPbnAh5YiV5VwcTLnGdwSNsNTn2aDxdXBFca7D/example#/ipfs/QmSsNVuALPa1TW1GDahup8fFDqo95iFyPE7E6HpqDivw3p/readme.md';
+const ipnsPath = 'ipfs.git.sexy';
 
 exports['test ipfsScheme'] = function(assert) {
   assert.equal(proto.ipfsScheme, 'ipfs', 'handler scheme');
 };
 
-// ipfs:<path> should be converted to Public Gateway URL
+// ipfs:<path> / ipns:<path> / ipfs://<path> / ipns://<path>
+// should be converted to Public Gateway URL
 // (which can be redirected to custom one in later steps)
+
 exports['test newURI(ipfs:<path>)'] = function(assert) {
-  var path = 'QmTkzDwWqPbnAh5YiV5VwcTLnGdwSNsNTn2aDxdXBFca7D/example#/ipfs/QmSsNVuALPa1TW1GDahup8fFDqo95iFyPE7E6HpqDivw3p/readme.md';
-  var newURI = proto.ipfsHandler.prototype.newURI('ipfs:' + path, 'utf8', null);
-  assert.equal(newURI.scheme, pubGwUri.scheme, 'newURI scheme');
-  assert.equal(newURI.host, pubGwUri.host, 'newURI host');
-  assert.equal(newURI.path, '/ipfs/' + path, 'newURI path');
-  assert.equal(newURI.spec, pubGwUri.spec + 'ipfs/' + path, 'newURI spec (full URL)');
+  var newURI = proto.ipfsHandler.prototype.newURI('ipfs:' + ipfsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipfs/' + ipfsPath, 'newURI spec');
 };
 
-// ipins:<path> should be converted to Public Gateway URL
-// (which can be redirected to custom one in later steps)
+exports['test newURI(ipfs://<path>)'] = function(assert) {
+  var newURI = proto.ipfsHandler.prototype.newURI('ipfs://' + ipfsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipfs/' + ipfsPath, 'newURI spec');
+};
+
+
 exports['test newURI(ipns:<path>)'] = function(assert) {
-  var path = 'ipfs.git.sexy';
-  var newURI = proto.ipnsHandler.prototype.newURI('ipns:' + path, 'utf8', null);
-  assert.equal(newURI.scheme, pubGwUri.scheme, 'newURI scheme');
-  assert.equal(newURI.host, pubGwUri.host, 'newURI host');
-  assert.equal(newURI.path, '/ipns/' + path, 'newURI path');
-  assert.equal(newURI.spec, pubGwUri.spec + 'ipns/' + path, 'newURI spec (full URL)');
+  var newURI = proto.ipnsHandler.prototype.newURI('ipns:' + ipnsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipns/' + ipnsPath, 'newURI spec');
+};
+
+exports['test newURI(ipns:<path>)'] = function(assert) {
+  var newURI = proto.ipnsHandler.prototype.newURI('ipns://' + ipnsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipns/' + ipnsPath, 'newURI spec');
 };
 
 
