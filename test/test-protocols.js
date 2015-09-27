5d52c5cc5ad23385485abe501d6e6eae8d767e8f
Marcin Rataj <lidel@lidel.org>
2015-09-27 15:15:17 +0200

    Support for the fs: protocol handler
    - supports all `/` combinations (think `fs:/*<resource>`)
    - discussed at length in github thread, the gist being:
      https://github.com/ipfs/go-ipfs/issues/1678#issuecomment-142600157

--- test/test-protocols.js
+++ test/test-protocols.js
@@ -8,6 +8,14 @@ exports['test ipfsScheme'] = function(assert) {
   assert.equal(proto.ipfsScheme, 'ipfs', 'handler scheme');
 };
 
+exports['test ipnsScheme'] = function(assert) {
+  assert.equal(proto.ipnsScheme, 'ipns', 'handler scheme');
+};
+
+exports['test fsScheme'] = function(assert) {
+  assert.equal(proto.fsScheme, 'fs', 'handler scheme');
+};
+
 // ipfs:<path> / ipns:<path> / ipfs://<path> / ipns://<path>
 // should be converted to Public Gateway URL
 // (which can be redirected to custom one in later steps)
@@ -30,6 +38,50 @@ exports['test newURI(ipns:<path>)'] = function(assert) {
 
 exports['test newURI(ipns:<path>)'] = function(assert) {
   var newURI = proto.ipnsHandler.prototype.newURI('ipns://' + ipnsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipns/' + ipnsPath, 'newURI spec');
+};
+
+// The fs:<resource> protocol for easier interoperability with legacy applications
+// It is a simple prefix to the canonical UNIX-like IPFS address
+// Discussed at length in https://github.com/ipfs/go-ipfs/issues/1678#issuecomment-142600157
+
+exports['test newURI(fs:ipfs/<path>)'] = function(assert) {
+  var newURI = proto.fsHandler.prototype.newURI('fs:ipfs/' + ipfsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipfs/' + ipfsPath, 'newURI spec');
+};
+
+exports['test newURI(fs:/ipfs/<path>)'] = function(assert) {
+  var newURI = proto.fsHandler.prototype.newURI('fs:/ipfs/' + ipfsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipfs/' + ipfsPath, 'newURI spec');
+};
+
+exports['test newURI(fs://ipfs/<path>)'] = function(assert) {
+  var newURI = proto.fsHandler.prototype.newURI('fs://ipfs/' + ipfsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipfs/' + ipfsPath, 'newURI spec');
+};
+
+exports['test newURI(fs:///ipfs/<path>)'] = function(assert) {
+  var newURI = proto.fsHandler.prototype.newURI('fs:///ipfs/' + ipfsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipfs/' + ipfsPath, 'newURI spec');
+};
+
+exports['test newURI(fs:ipns/<path>)'] = function(assert) {
+  var newURI = proto.fsHandler.prototype.newURI('fs:ipns/' + ipnsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipns/' + ipnsPath, 'newURI spec');
+};
+
+exports['test newURI(fs:/ipns/<path>)'] = function(assert) {
+  var newURI = proto.fsHandler.prototype.newURI('fs:/ipns/' + ipnsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipns/' + ipnsPath, 'newURI spec');
+};
+
+exports['test newURI(fs://ipns/<path>)'] = function(assert) {
+  var newURI = proto.fsHandler.prototype.newURI('fs://ipns/' + ipnsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipns/' + ipnsPath, 'newURI spec');
+};
+
+exports['test newURI(fs:///ipns/<path>)'] = function(assert) {
+  var newURI = proto.fsHandler.prototype.newURI('fs:///ipns/' + ipnsPath, 'utf8', null);
   assert.equal(newURI.spec, pubGwUri.spec + 'ipns/' + ipnsPath, 'newURI spec');
 };
 
