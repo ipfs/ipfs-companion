6187aa41d758655ce1a7e586fc7aa71210f36986
Marcin Rataj <lidel@lidel.org>
2015-12-27 19:53:03 +0100

    Support for "web+" protocol prefix
    - closes #36
    - follows consensus from
      https://github.com/ipfs/go-ipfs/issues/1678#issuecomment-163124489

--- test/test-protocols.js
+++ test/test-protocols.js
@@ -4,16 +4,32 @@ const pubGwUri = gw.publicUri();
 const ipfsPath = 'QmTkzDwWqPbnAh5YiV5VwcTLnGdwSNsNTn2aDxdXBFca7D/example#/ipfs/QmSsNVuALPa1TW1GDahup8fFDqo95iFyPE7E6HpqDivw3p/readme.md';
 const ipnsPath = 'ipfs.git.sexy';
 
-exports['test ipfsScheme'] = function(assert) {
-  assert.equal(proto.ipfsScheme, 'ipfs', 'handler scheme');
+const ipfsHandler = proto.ipfs.createInstance();
+const ipnsHandler = proto.ipns.createInstance();
+const fsHandler   = proto.fs.createInstance();
+
+const webIpfsHandler = proto['web+ipfs'].createInstance();
+const webIpnsHandler = proto['web+ipns'].createInstance();
+const webFsHandler   = proto['web+fs'].createInstance();
+
+exports['test ipfs: scheme'] = function(assert) {
+  assert.equal(ipfsHandler.scheme, 'ipfs', 'handler scheme');
+};
+exports['test ipns: scheme'] = function(assert) {
+  assert.equal(ipnsHandler.scheme, 'ipns', 'handler scheme');
+};
+exports['test fs: scheme'] = function(assert) {
+  assert.equal(fsHandler.scheme, 'fs', 'handler scheme');
 };
 
-exports['test ipnsScheme'] = function(assert) {
-  assert.equal(proto.ipnsScheme, 'ipns', 'handler scheme');
+exports['test web+ipfs scheme'] = function(assert) {
+  assert.equal(webIpfsHandler.scheme, 'web+ipfs', 'handler scheme');
 };
-
-exports['test fsScheme'] = function(assert) {
-  assert.equal(proto.fsScheme, 'fs', 'handler scheme');
+exports['test web+ipns scheme'] = function(assert) {
+  assert.equal(webIpnsHandler.scheme, 'web+ipns', 'handler scheme');
+};
+exports['test web+fs scheme'] = function(assert) {
+  assert.equal(webFsHandler.scheme, 'web+fs', 'handler scheme');
 };
 
 // ipfs:<path> / ipns:<path> / ipfs://<path> / ipns://<path>
@@ -21,23 +37,37 @@ exports['test fsScheme'] = function(assert) {
 // (which can be redirected to custom one in later steps)
 
 exports['test newURI(ipfs:<path>)'] = function(assert) {
-  var newURI = proto.ipfsHandler.prototype.newURI('ipfs:' + ipfsPath, 'utf8', null);
+  var newURI = ipfsHandler.newURI('ipfs:' + ipfsPath, 'utf8', null);
   assert.equal(newURI.spec, pubGwUri.spec + 'ipfs/' + ipfsPath, 'newURI spec');
 };
-
 exports['test newURI(ipfs://<path>)'] = function(assert) {
-  var newURI = proto.ipfsHandler.prototype.newURI('ipfs://' + ipfsPath, 'utf8', null);
+  var newURI = ipfsHandler.newURI('ipfs://' + ipfsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipfs/' + ipfsPath, 'newURI spec');
+};
+exports['test newURI(web+ipfs:<path>)'] = function(assert) {
+  var newURI = webIpfsHandler.newURI('web+ipfs:' + ipfsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipfs/' + ipfsPath, 'newURI spec');
+};
+exports['test newURI(web+ipfs://<path>)'] = function(assert) {
+  var newURI = webIpfsHandler.newURI('web+ipfs://' + ipfsPath, 'utf8', null);
   assert.equal(newURI.spec, pubGwUri.spec + 'ipfs/' + ipfsPath, 'newURI spec');
 };
 
 
 exports['test newURI(ipns:<path>)'] = function(assert) {
-  var newURI = proto.ipnsHandler.prototype.newURI('ipns:' + ipnsPath, 'utf8', null);
+  var newURI = ipnsHandler.newURI('ipns:' + ipnsPath, 'utf8', null);
   assert.equal(newURI.spec, pubGwUri.spec + 'ipns/' + ipnsPath, 'newURI spec');
 };
-
-exports['test newURI(ipns:<path>)'] = function(assert) {
-  var newURI = proto.ipnsHandler.prototype.newURI('ipns://' + ipnsPath, 'utf8', null);
+exports['test newURI(ipns://<path>)'] = function(assert) {
+  var newURI = ipnsHandler.newURI('ipns://' + ipnsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipns/' + ipnsPath, 'newURI spec');
+};
+exports['test newURI(web+ipns:<path>)'] = function(assert) {
+  var newURI = webIpnsHandler.newURI('web+ipns:' + ipnsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipns/' + ipnsPath, 'newURI spec');
+};
+exports['test newURI(web+ipns://<path>)'] = function(assert) {
+  var newURI = webIpnsHandler.newURI('web+ipns://' + ipnsPath, 'utf8', null);
   assert.equal(newURI.spec, pubGwUri.spec + 'ipns/' + ipnsPath, 'newURI spec');
 };
 
@@ -46,44 +76,86 @@ exports['test newURI(ipns:<path>)'] = function(assert) {
 // Discussed at length in https://github.com/ipfs/go-ipfs/issues/1678#issuecomment-142600157
 
 exports['test newURI(fs:ipfs/<path>)'] = function(assert) {
-  var newURI = proto.fsHandler.prototype.newURI('fs:ipfs/' + ipfsPath, 'utf8', null);
+  var newURI = fsHandler.newURI('fs:ipfs/' + ipfsPath, 'utf8', null);
   assert.equal(newURI.spec, pubGwUri.spec + 'ipfs/' + ipfsPath, 'newURI spec');
 };
 
 exports['test newURI(fs:/ipfs/<path>)'] = function(assert) {
-  var newURI = proto.fsHandler.prototype.newURI('fs:/ipfs/' + ipfsPath, 'utf8', null);
+  var newURI = fsHandler.newURI('fs:/ipfs/' + ipfsPath, 'utf8', null);
   assert.equal(newURI.spec, pubGwUri.spec + 'ipfs/' + ipfsPath, 'newURI spec');
 };
 
 exports['test newURI(fs://ipfs/<path>)'] = function(assert) {
-  var newURI = proto.fsHandler.prototype.newURI('fs://ipfs/' + ipfsPath, 'utf8', null);
+  var newURI = fsHandler.newURI('fs://ipfs/' + ipfsPath, 'utf8', null);
   assert.equal(newURI.spec, pubGwUri.spec + 'ipfs/' + ipfsPath, 'newURI spec');
 };
 
 exports['test newURI(fs:///ipfs/<path>)'] = function(assert) {
-  var newURI = proto.fsHandler.prototype.newURI('fs:///ipfs/' + ipfsPath, 'utf8', null);
+  var newURI = fsHandler.newURI('fs:///ipfs/' + ipfsPath, 'utf8', null);
   assert.equal(newURI.spec, pubGwUri.spec + 'ipfs/' + ipfsPath, 'newURI spec');
 };
 
 exports['test newURI(fs:ipns/<path>)'] = function(assert) {
-  var newURI = proto.fsHandler.prototype.newURI('fs:ipns/' + ipnsPath, 'utf8', null);
+  var newURI = fsHandler.newURI('fs:ipns/' + ipnsPath, 'utf8', null);
   assert.equal(newURI.spec, pubGwUri.spec + 'ipns/' + ipnsPath, 'newURI spec');
 };
 
 exports['test newURI(fs:/ipns/<path>)'] = function(assert) {
-  var newURI = proto.fsHandler.prototype.newURI('fs:/ipns/' + ipnsPath, 'utf8', null);
+  var newURI = fsHandler.newURI('fs:/ipns/' + ipnsPath, 'utf8', null);
   assert.equal(newURI.spec, pubGwUri.spec + 'ipns/' + ipnsPath, 'newURI spec');
 };
 
 exports['test newURI(fs://ipns/<path>)'] = function(assert) {
-  var newURI = proto.fsHandler.prototype.newURI('fs://ipns/' + ipnsPath, 'utf8', null);
+  var newURI = fsHandler.newURI('fs://ipns/' + ipnsPath, 'utf8', null);
   assert.equal(newURI.spec, pubGwUri.spec + 'ipns/' + ipnsPath, 'newURI spec');
 };
 
 exports['test newURI(fs:///ipns/<path>)'] = function(assert) {
-  var newURI = proto.fsHandler.prototype.newURI('fs:///ipns/' + ipnsPath, 'utf8', null);
+  var newURI = fsHandler.newURI('fs:///ipns/' + ipnsPath, 'utf8', null);
   assert.equal(newURI.spec, pubGwUri.spec + 'ipns/' + ipnsPath, 'newURI spec');
 };
+
+// web+ variant (https://github.com/lidel/ipfs-firefox-addon/issues/36)
+exports['test newURI(web+fs:ipfs/<path>)'] = function(assert) {
+  var newURI = webFsHandler.newURI('web+fs:ipfs/' + ipfsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipfs/' + ipfsPath, 'newURI spec');
+};
+
+exports['test newURI(web+fs:/ipfs/<path>)'] = function(assert) {
+  var newURI = webFsHandler.newURI('web+fs:/ipfs/' + ipfsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipfs/' + ipfsPath, 'newURI spec');
+};
+
+exports['test newURI(web+fs://ipfs/<path>)'] = function(assert) {
+  var newURI = webFsHandler.newURI('web+fs://ipfs/' + ipfsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipfs/' + ipfsPath, 'newURI spec');
+};
+
+exports['test newURI(web+fs:///ipfs/<path>)'] = function(assert) {
+  var newURI = webFsHandler.newURI('web+fs:///ipfs/' + ipfsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipfs/' + ipfsPath, 'newURI spec');
+};
+
+exports['test newURI(web+fs:ipns/<path>)'] = function(assert) {
+  var newURI = webFsHandler.newURI('web+fs:ipns/' + ipnsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipns/' + ipnsPath, 'newURI spec');
+};
+
+exports['test newURI(web+fs:/ipns/<path>)'] = function(assert) {
+  var newURI = webFsHandler.newURI('web+fs:/ipns/' + ipnsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipns/' + ipnsPath, 'newURI spec');
+};
+
+exports['test newURI(web+fs://ipns/<path>)'] = function(assert) {
+  var newURI = webFsHandler.newURI('web+fs://ipns/' + ipnsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipns/' + ipnsPath, 'newURI spec');
+};
+
+exports['test newURI(web+fs:///ipns/<path>)'] = function(assert) {
+  var newURI = webFsHandler.newURI('web+fs:///ipns/' + ipnsPath, 'utf8', null);
+  assert.equal(newURI.spec, pubGwUri.spec + 'ipns/' + ipnsPath, 'newURI spec');
+};
+
 
 
 require('sdk/test').run(exports);
