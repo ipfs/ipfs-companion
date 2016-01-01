6e83092a3a511ed9e74dfe79fad6356afc06d2c5
Marcin Rataj <lidel@lidel.org>
2016-01-02 00:20:09 +0100

    UX: First stab at the universal button-menu
    - functional work-in-progress
    - implements #32
    - shares code with context menu
        - added link to open webui (uses API port of go-ipfs)
        - redirect toggle is now just one of panel entries
        - some entries can be displayed only
          for IPFS resources
    
    TODO: add regression tests where possible

---
+++ data/panel.js
@@ -0,0 +1,27 @@
+// incoming
+self.port.on('show', function(ipfsResource) {
+  //console.log('show event received by panel.js (ipfsResource='+ipfsResource+')');
+
+  // show/hide ipfs-only items
+  document.getElementById('ipfs-only')
+    .style.display = ipfsResource ? 'block' : 'none';
+});
+
+// outgoing
+function forwardClickEvent(eventName) {
+  document.getElementById(eventName)
+    .addEventListener('click', function(event) {
+      self.port.emit(eventName);
+  });
+}
+
+// always visible
+forwardClickEvent('toggle-gateway-redirect');
+forwardClickEvent('open-webui');
+
+// ipfs-only
+forwardClickEvent('pin-current-ipfs-address');
+forwardClickEvent('copy-current-ipfs-address');
+forwardClickEvent('copy-current-public-gw-url');
+
+
--- test/test-gui.js
+++ test/test-gui.js
@@ -1,21 +1,49 @@
+
 var gui = require('../lib/gui.js');
 var gw = require('../lib/gateways.js');
 var prefs = require('sdk/simple-prefs').prefs;
-const button = gui.toggleButton;
 
-exports['test toggleButton attributes'] = function(assert) {
+var button = gui.toggleButton;
+var panel = gui.toggleButtonPanel;
+
+// BUTTON BADGE
+exports['test default toggleButton attributes'] = function(assert) {
   assert.equal(button.id, 'ipfs-gateway-status', 'button id');
-  assert.equal(button.checked, prefs.useCustomGateway, 'default state equal prefs.useCustomGateway');
+  assert.equal(button.badge, prefs.useCustomGateway ? 'ON' : 'OFF', 'default badge follows prefs.useCustomGateway');
 };
-
 exports['test disabled toggleButton'] = function(assert) {
   gw.toggle(false);
-  assert.equal(button.checked, prefs.useCustomGateway, 'state after gw.disableHttpGatewayRedirect()');
+  assert.equal(button.badge, 'OFF', 'badge for prefs.useCustomGateway=false');
 };
-
 exports['test enabled toggleButton'] = function(assert) {
   gw.toggle(true);
-  assert.equal(button.checked, prefs.useCustomGateway, 'state after gw.enableHttpGatewayRedirect()');
+  assert.equal(button.badge, 'ON', 'badge for prefs.useCustomGateway=false');
 };
+
+exports['test toggleButtonPanel default state'] = function(assert) {
+  assert.equal(panel.isShowing, false, 'panel should be hidden by default');
+};
+
+/* TODO: test onclick state
+exports['test toggleButton & Panel onClick state'] = function(assert, done) {
+  let { waitUntil } = require('sdk/test/utils');
+
+  assert.equal(panel.isShowing, false, 'panel should be hidden by default');
+
+  button.click();
+
+  console.log('before waitUntil');
+  waitUntil(function() {
+    console.log('run waitUntil');
+    return panel.isShowing;
+  }, 100).then(function() {
+    console.log('run waitUntil.then');
+    assert.equal(panel.isShowing, true, 'panel should be displayed after toggleButton click event');
+    done();
+  });
+  console.log('after waitUntil');
+
+};
+*/
 
 require('sdk/test').run(exports);
