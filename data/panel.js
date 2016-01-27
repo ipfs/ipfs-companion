e532520c96d24bcbf70321e57426b382037a8135
Marcin Rataj <lidel@lidel.org>
2016-01-27 23:12:27 +0100

    Gateway Diagnostics PoC
    - discussed in #51
    - FEATURE: version and swarm peer info read from API
    - UX: hide irrelevant elements when redirect is disabled

--- data/panel.js
+++ data/panel.js
@@ -1,32 +1,65 @@
 /* global self */
+let getById = (id) => {
+  return document.getElementById(id)
+}
+
+let renderGatewayAddress = (prefs) => {
+  if (prefs.useCustomGateway) {
+    return prefs.customGatewayHost + ':' + prefs.customGatewayPort
+  } else {
+    return 'OFF'
+  }
+}
+
+function showIf(id, condition) {
+  getById(id).style.display = condition ? 'block' : 'none'
+}
 
 // incoming
 self.port.on('show', function (context) {
   // console.log('show event received by panel.js (ipfsResource='+ipfsResource+')')
-  let isPinnable = context['isPinnable']
+  let prefs = context.preferences
 
-  // show/hide ipfs-only items
-  document.getElementById('ipfs-only')
-    .style.display = isPinnable ? 'block' : 'none'
+  // render diagnostic info
+  getById('gateway-address-val').innerHTML = renderGatewayAddress(prefs)
 
-  // match panel size
+  // if current page is pinnable
+  showIf('ipfs-resource-actions', context.isPinnable)
+
+  // if custom gateway is used
+  showIf('open-webui', prefs.useCustomGateway)
+  showIf('gateway-version', prefs.useCustomGateway)
+  showIf('swarm-peers', prefs.useCustomGateway)
+  showIf('pin-current-ipfs-address', prefs.useCustomGateway)
+
+  // resize panel to match size of rendered items
   self.port.emit('resize', { 'width': document.body.scrollWidth, 'height': document.body.scrollHeight })
+})
+
+self.port.on('version', function (update) {
+  getById('gateway-version-val').innerHTML = update
+    ? update.Version + '-' + update.Commit
+    : 'n/a'
+})
+
+self.port.on('swarm-peers', function (update) {
+  getById('swarm-peers-val').innerHTML = (update && update.Strings)
+    ? update.Strings.length
+    : 'n/a'
 })
 
 // outgoing
 function forwardClickEvent (eventName) {
-  document.getElementById(eventName)
+  getById(eventName)
     .addEventListener('click', function (event) { // eslint-disable-line no-unused-vars
       self.port.emit(eventName)
     })
 }
 
-// always visible
 forwardClickEvent('toggle-gateway-redirect')
 forwardClickEvent('open-webui')
 forwardClickEvent('open-preferences')
 
-// ipfs-only
 forwardClickEvent('pin-current-ipfs-address')
 forwardClickEvent('copy-current-ipfs-address')
 forwardClickEvent('copy-current-public-gw-url')
