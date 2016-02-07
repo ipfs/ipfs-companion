e343854e54e0d44e27048c752515806c02df972b
Marcin Rataj <lidel@lidel.org>
2016-02-07 16:54:49 +0100

    Run the latest eslint-standard without grunt wrapper

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,18 +2,19 @@ module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt)
   var jsPaths = ['*.js', 'lib/**.js', 'test/**.js', 'data/**.js']
   grunt.initConfig({
-    eslint: {
-      target: jsPaths
-    },
     run: {
       jpm_test: {
         cmd: 'jpm',
         args: ['-b', process.env.FIREFOX_BIN || 'firefox', 'test']
+      },
+      eslint: {
+        cmd: 'eslint',
+        args: jsPaths
       }
     }
   })
 
-  grunt.registerTask('test', ['run:jpm_test', 'eslint'])
+  grunt.registerTask('test', ['run:jpm_test', 'run:eslint'])
   grunt.registerTask('default', ['test'])
 }
 // vim:ts=2:sw=2:et:
