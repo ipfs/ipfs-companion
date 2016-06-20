9b30d4492ee03ada79d6a4171ca24c79c07c1c62
Marcin Rataj <lidel@lidel.org>
2016-06-20 21:51:23 +0200

    Switch to updated SVG logo
    - custom version of -off icon
    - closes #104

--- data/panel.js
+++ data/panel.js
@@ -15,7 +15,7 @@ function showIf (id, condition) {
 }
 
 function setIconState (enable) {
-  getById('icon').src = enable ? 'icon-on-64.png' : 'icon-off-64.png'
+  getById('icon').src = enable ? 'ipfs-logo-on.svg' : 'ipfs-logo-off.svg'
 }
 
 // incoming
