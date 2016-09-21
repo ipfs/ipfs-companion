9efe842baf76af6879cbe501ce6f63103a7ed6f0
Marcin Rataj <lidel@lidel.org>
2016-09-21 22:52:36 +0200

    Improvements in saving/restoring settings
    - simplified initialization of default values
    - validation via HTML5
    - #20

--- settings/options.js
+++ settings/options.js
@@ -1,14 +1,55 @@
-function saveOptions(e) {
-  chrome.storage.local.set({
-    publicGateways: document.querySelector("#publicGateways").value
-  });
+var options = new Map() // key: optionName, value: defaultValue
+options.set('publicGateways', 'ipfs.io gateway.ipfs.io ipfs.pics')
+options.set('useCustomGateway', true)
+options.set('customGatewayUrl', 'http://127.0.0.1:8080')
+
+
+function saveOption (name) {
+  let element = document.querySelector(`#${name}`)
+  let change = {}
+  switch (element.type) {
+    case 'text':
+    case 'url':
+      change[name] = element.value
+      break
+    case 'checkbox':
+      change[name] = element.checked
+      break
+    default:
+      console.log('Unsupported option type: ' + element.type)
+  }
+  chrome.storage.local.set(change)
 }
 
-function restoreOptions() {
-  chrome.storage.local.get("publicGateways", (res) => {
-    document.querySelector("#publicGateways").value = res.publicGateways || "ipfs.io gateway.ipfs.io ipfs.pics";
-  });
+function readOption (name, defaultValue) {
+  let element = document.querySelector(`#${name}`)
+  chrome.storage.local.get(name, (storage) => {
+    let oldValue = storage[name]
+    switch (element.type) {
+      case 'text':
+      case 'url':
+        element.value = oldValue || defaultValue
+        break
+      case 'checkbox':
+        element.checked = typeof (oldValue) === 'boolean' ? oldValue : defaultValue
+        break
+      default:
+        console.log('Unsupported option type: ' + element.type)
+    }
+  })
 }
 
-document.addEventListener("DOMContentLoaded", restoreOptions);
-document.querySelector("form").addEventListener("submit", saveOptions);
+function saveOptions (e) {
+  for (var option of options.keys()) {
+    saveOption(option)
+  }
+}
+
+function readOptions () {
+  for (var [option, defaultValue] of options) {
+    readOption(option, defaultValue)
+  }
+}
+
+document.addEventListener('DOMContentLoaded', readOptions)
+document.querySelector('form').addEventListener('submit', saveOptions)
