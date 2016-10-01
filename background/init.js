81467f2ff1efdb2f19ea37a7c4708e6ca323998a
Marcin Rataj <lidel@lidel.org>
2016-10-01 14:54:56 +0200

    PoC inclusion of browserified is-ipfs

--- background/init.js
+++ background/init.js
@@ -1,13 +1,15 @@
-function initPoC() {
-  let ipfs = window.IpfsApi({host: '127.0.0.1', port: '5001', procotol: 'http'});
+function initPoC () {
+  let isIpfs = window.IsIpfs
+  console.log('is-ipfs ------> ' + isIpfs.multihash('QmUqRvxzQyYWNY6cD1Hf168fXeqDTQWwZpyXjU5RUExciZ'))
+
+  let ipfs = window.IpfsApi({host: '127.0.0.1', port: '5001', procotol: 'http'})
   ipfs.id()
     .then(function (id) {
-      console.log('--------> my id is: ', id)
+      console.log('js-ipfs-api --------> my id is: ', id)
     })
-    .catch(function(err) {
-      console.log('--------> Fail: ', err)
+    .catch(function (err) {
+      console.log('ja-ipfs-api --------> Fail: ', err)
     })
 }
 
-
-initPoC();
+initPoC()
