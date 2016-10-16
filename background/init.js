b6dbbdeb6f36452ae61781ca271b66c86e72d079
Marcin Rataj <lidel@lidel.org>
2016-10-16 19:06:06 +0200

    FIX: sandboxed loading of browserified libraries
    - background: https://git.io/vPiVp
    - closes #152

--- background/init.js
+++ background/init.js
@@ -1,15 +1,30 @@
-function initPoC () {
-  let isIpfs = window.IsIpfs
-  console.log('is-ipfs ------> ' + isIpfs.multihash('QmUqRvxzQyYWNY6cD1Hf168fXeqDTQWwZpyXjU5RUExciZ'))
+var ipfs, isIpfs
 
-  let ipfs = window.IpfsApi({host: '127.0.0.1', port: '5001', procotol: 'http'})
+function initPoC () {
+  console.log('init.js::initPoC started..')
+  ipfs = initIpfsApi()
   ipfs.id()
     .then(function (id) {
-      console.log('js-ipfs-api --------> my id is: ', id)
+      console.log('ipfs-api --------> Node ID is: ', id)
     })
     .catch(function (err) {
-      console.log('ja-ipfs-api --------> Fail: ', err)
+      console.log('ipfs-api --------> Fail: ', err)
     })
+
+  isIpfs = initIsIpfs()
+  console.log('is-ipfs test (should be true) ------> ' + isIpfs.multihash('QmUqRvxzQyYWNY6cD1Hf168fXeqDTQWwZpyXjU5RUExciZ'))
+
 }
 
-initPoC()
+
+function initIpfsApi() {
+  let ipfsApi = window.frames['ipfsApiSandbox'].IpfsApi
+  return ipfsApi({host: '127.0.0.1', port: '5001', procotol: 'http'})
+}
+
+function initIsIpfs() {
+  return window.frames['isIpfsSandbox'].IsIpfs
+}
+
+window.onload = initPoC
+console.log('init.js finished')
