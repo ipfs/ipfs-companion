d36cf5554e414e96e2c6018e3bacb4d418352b63
Marcin Rataj <lidel@lidel.org>
2016-10-29 21:05:48 +0200

    TravisCI: remove dependency on Aria2

--- scripts/fetch-unbranded.sh
+++ scripts/fetch-unbranded.sh
@@ -6,4 +6,4 @@ ROOT="https://archive.mozilla.org"
 LATEST=$(curl -s "$ROOT$BUILD_ROOT" | grep $BUILD_TYPE | grep -Po '<a href=".+">\K[[:digit:]]+' | sort -n | tail -1)
 FILE=$(curl -s "$ROOT$BUILD_ROOT$LATEST/" | grep '.tar.' | grep -Po '<a href="\K[^"]*')
 echo "URL: $ROOT$FILE"
-exec aria2c -q -o "firefox-${BUILD_TYPE}.tar.bz2" "$ROOT$FILE"
+exec wget -O "firefox-${BUILD_TYPE}.tar.bz2" "$ROOT$FILE"
