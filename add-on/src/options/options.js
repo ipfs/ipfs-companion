46dccddf8769e505b07827f19a78d30659cb2917
Marcin Rataj <lidel@lidel.org>
2016-12-18 17:51:08 +0100

    refactor: replace callback with promise

--- add-on/src/options/options.js
+++ add-on/src/options/options.js
@@ -26,24 +26,23 @@ function readOption (name) {
   if (element) {
     browser.storage.local.get(name)
       .then(storage => {
-        if (browser.runtime.lastError) {
-          console.log(browser.runtime.lastError)
-        } else {
-          const oldValue = storage[name]
-          switch (element.type) {
-            case 'text':
-            case 'url':
-              element.value = oldValue
-              break
-            case 'checkbox':
-              element.checked = typeof (oldValue) === 'boolean' ? oldValue : false
-              break
-            default:
-              console.log('Unsupported option type: ' + element.type)
-          }
-          element.onblur = () => saveOption(name) // autosave
+        const oldValue = storage[name]
+        switch (element.type) {
+          case 'text':
+          case 'url':
+            element.value = oldValue
+            break
+          case 'checkbox':
+            element.checked = typeof (oldValue) === 'boolean' ? oldValue : false
+            break
+          default:
+            console.log('Unsupported option type: ' + element.type)
         }
+        element.onblur = () => saveOption(name) // autosave
       })
+    .catch(error => {
+      console.error(`Unable to initialize oprions due to ${error}`)
+    })
   }
 }
 
