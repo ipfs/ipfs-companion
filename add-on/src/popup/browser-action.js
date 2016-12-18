66875d5c4fe86018016265f54618de3242dfa0ed
Marcin Rataj <lidel@lidel.org>
2016-12-18 15:11:42 +0100

    Listen to API changes and update diagnostics in realtime

--- add-on/src/popup/browser-action.js
+++ add-on/src/popup/browser-action.js
@@ -9,39 +9,46 @@ function show (id) {
   document.getElementById(id).style = 'display:inline-block'
 }
 
-// update redirect status
-browser.storage.local.get('useCustomGateway')
-  .then(options => {
-    const enabled = options['useCustomGateway']
-    if (enabled) {
-      show('redirect-enabled')
-      show('disable-gateway-redirect')
-    } else {
-      show('redirect-disabled')
-      show('enable-gateway-redirect')
-    }
-  })
+function updateDiagnostics () {
+  // update redirect status
+  browser.storage.local.get('useCustomGateway')
+    .then(options => {
+      const enabled = options['useCustomGateway']
+      if (enabled) {
+        show('redirect-enabled')
+        show('disable-gateway-redirect')
+      } else {
+        show('redirect-disabled')
+        show('enable-gateway-redirect')
+      }
+    })
 
-// update gateway addresss
-browser.storage.local.get('customGatewayUrl')
-  .then(options => { set('gateway-address-val', options['customGatewayUrl']) })
-  .catch(() => { set('gateway-address-val', '???') })
+  // update gateway addresss
+  browser.storage.local.get('customGatewayUrl')
+    .then(options => { set('gateway-address-val', options['customGatewayUrl']) })
+    .catch(() => { set('gateway-address-val', '???') })
 
-browser.runtime.getBackgroundPage()
-  .then(background => {
-    // update gateway version
-    background.ipfs.version()
-      .then(v => { set('gateway-version-val', (v.commit ? v.version + '/' + v.commit : v.version)) })
-      .catch(() => { set('gateway-version-val', 'offline') })
+  browser.runtime.getBackgroundPage()
+    .then(background => {
+      // update gateway version
+      background.ipfs.version()
+        .then(v => { set('gateway-version-val', (v.commit ? v.version + '/' + v.commit : v.version)) })
+        .catch(() => { set('gateway-version-val', 'offline') })
+      // update swarm peer count
+      background.getSwarmPeerCount()
+        .then(peerCount => {
+          if (peerCount < 0) {
+            set('swarm-peers-val', 'offline')
+          } else { set('swarm-peers-val', peerCount) }
+        })
+    })
+    .catch(error => {
+      console.error(`Unable to access background page due to ${error}`)
+    })
+}
 
-    // update swarm peer count
-    background.getSwarmPeerCount()
-      .then(peerCount => {
-        if (peerCount < 0) {
-          set('swarm-peers-val', 'offline')
-        } else { set('swarm-peers-val', peerCount) }
-      })
-  })
-  .catch(error => {
-    console.error(`Unable to access background page due to ${error}`)
-  })
+// run on initial popup load
+updateDiagnostics()
+
+// listen to any changes and update diagnostics
+browser.alarms.onAlarm.addListener(updateDiagnostics)
