e4c8ade6fc6b63c63cb6482ef3bdf490d45cd13d
Marcin Rataj <lidel@lidel.org>
2017-02-12 14:56:09 +0100

    refactor(i18n): return name of missing key instead of empty string

--- add-on/src/lib/data-i18n.js
+++ add-on/src/lib/data-i18n.js
@@ -1,14 +1,22 @@
 'use strict'
 /* eslint-env browser, webextensions */
 
+function safeTranslation (key) {
+  const translation = browser.i18n.getMessage(key)
+  return translation || '[i18n: ' + key + ']'
+}
+
 // Search for items with data-i18n attribute and replace them with values from current locale
-let items = document.querySelectorAll('[data-i18n]')
+const items = document.querySelectorAll('[data-i18n]')
 for (let item of items) {
-  let translation = browser.i18n.getMessage(item.getAttribute('data-i18n'))
-  if (typeof item.value !== 'undefined' && item.value === 'i18n') {
-    // things like inputs can trigger translation with value equal "i18n"
-    item.value = translation
-  } else {
-    item.innerText = translation
+  const key = item.getAttribute('data-i18n')
+  if (key) {
+    const translation = safeTranslation(key)
+    if (typeof item.value !== 'undefined' && item.value === 'i18n') {
+        // things like inputs can trigger translation with value equal "i18n"
+      item.value = translation
+    } else {
+      item.innerText = translation
+    }
   }
 }
