c34659b3d923744f2f2e00314daa4bf6a4c04a5d
Marcin Rataj <lidel@lidel.org>
2017-11-20 20:35:47 +0100

    chore(test): extract PoC integration tests
    Ref.
    https://github.com/ipfs/ipfs-companion/pull/311#issuecomment-345755680
    https://github.com/ipfs/ipfs-companion/issues/165

---
+++ test/integration-firefox/README.md
@@ -0,0 +1,1 @@
+See https://github.com/ipfs/ipfs-companion/issues/165
--- test/unit/browser.shim.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-/* eslint-env browser, webextensions */
-/* eslint no-use-before-define: 0 */
-
-// without this shim unit tests fail -- sinon-chrome does not stub browser yet
-var browser = browser || chrome
