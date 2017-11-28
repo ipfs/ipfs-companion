93fbdb8b161a6695d312f32074305ac755478ef0
Alan Shaw <alan@tableflip.io>
2017-11-28 14:01:49 +0000

    adds popup header and adds browserify-css and tachyons

--- add-on/src/popup/browser-action/gateway-status.js
+++ add-on/src/popup/browser-action/gateway-status.js
@@ -12,35 +12,29 @@ module.exports = function gatewayStatus ({
   redirectEnabled
 }) {
   return html`
-    <div class="panel-section" id="gateway-status">
-      <img
-        id="icon"
-        src="../../../icons/ipfs-logo-${isIpfsOnline ? 'on' : 'off'}.svg"
-        class="${isIpfsOnline ? 'online' : ''}"/>
-      <ul>
-        <li>
-          <span>${browser.i18n.getMessage('panel_statusGatewayAddress')}</span>
-          <span>${gatewayAddress == null ? 'unknown' : gatewayAddress}</span>
-        </li>
-        <li>
-          <span>${browser.i18n.getMessage('panel_statusGatewayVersion')}</span>
-          <span>${gatewayVersion == null ? 'offline' : gatewayVersion}</span>
-        </li>
-        <li>
-          <span>${browser.i18n.getMessage('panel_statusSwarmPeers')}</span>
-          <span>${swarmPeers == null ? 'offline' : swarmPeers}</span>
-        </li>
-        <li>
-          <span>${browser.i18n.getMessage('panel_statusGatewayRedirect')}</span>
-          <span>
-            ${browser.i18n.getMessage(
-              redirectEnabled
-                ? 'panel_statusGatewayRedirectEnabled'
-                : 'panel_statusGatewayRedirectDisabled'
-            )}
-          </span>
-        </li>
-      </ul>
-    </div>
+    <ul class="list mv3 ph3">
+      <li class="flex mb1">
+        <span class="w-50 f6 ttu">${browser.i18n.getMessage('panel_statusGatewayAddress')}</span>
+        <span class="w-50 f6 ttu tr">${gatewayAddress == null ? 'unknown' : gatewayAddress}</span>
+      </li>
+      <li class="flex mb1">
+        <span class="w-50 f6 ttu">${browser.i18n.getMessage('panel_statusGatewayVersion')}</span>
+        <span class="w-50 f6 ttu tr">${gatewayVersion == null ? 'offline' : gatewayVersion}</span>
+      </li>
+      <li class="flex mb1">
+        <span class="w-50 f6 ttu">${browser.i18n.getMessage('panel_statusSwarmPeers')}</span>
+        <span class="w-50 f6 ttu tr">${swarmPeers == null ? 'offline' : swarmPeers}</span>
+      </li>
+      <li class="flex">
+        <span class="w-50 f6 ttu">${browser.i18n.getMessage('panel_statusGatewayRedirect')}</span>
+        <span class="w-50 f6 ttu tr">
+          ${browser.i18n.getMessage(
+            redirectEnabled
+              ? 'panel_statusGatewayRedirectEnabled'
+              : 'panel_statusGatewayRedirectDisabled'
+          )}
+        </span>
+      </li>
+    </ul>
   `
 }
