9aa0f8e4b329065c0263aaef201b97068a3679dc
Alan Shaw <alan@tableflip.io>
2018-01-24 16:33:28 +0000

    adds comments and reorganises code for better readability

--- add-on/src/lib/ipfs-proxy/index.js
+++ add-on/src/lib/ipfs-proxy/index.js
@@ -5,6 +5,8 @@ const browser = require('webextension-polyfill')
 const { createProxyServer, closeProxyServer } = require('ipfs-postmsg-proxy')
 const AccessControl = require('./access-control')
 
+// These are the functions that require an allow/deny decision by the user.
+// All other exposed IPFS functions are available to call without authorization.
 const ACL_FUNCTIONS = [
   'block.put',
   'config.set',
@@ -31,41 +33,14 @@ const ACL_FUNCTIONS = [
   'swarm.disconnect'
 ]
 
+// Creates an object that manages the "server side" of the IPFS proxy
 function createIpfsProxy (getIpfs, getState) {
   let connections = []
   const accessControl = new AccessControl(browser.storage)
 
-  const onPortConnect = (port) => {
-    if (port.name !== 'ipfs-proxy') return
-
-    const { origin } = new URL(port.sender.url)
-
-    const proxy = createProxyServer(getIpfs, {
-      addListener: (_, handler) => port.onMessage.addListener(handler),
-      removeListener: (_, handler) => port.onMessage.removeListener(handler),
-      postMessage: (data) => port.postMessage(data),
-      getMessageData: (d) => d,
-      pre: ACL_FUNCTIONS.reduce((obj, permission) => {
-        obj[permission] = createAclPreCall(accessControl, origin, permission)
-        return obj
-      }, {})
-    })
-
-    const close = () => {
-      closeProxyServer(proxy)
-      port.onDisconnect.removeListener(onDisconnect)
-      connections = connections.filter(c => c.close !== close)
-    }
-
-    const onDisconnect = () => close()
-
-    port.onDisconnect.addListener(onDisconnect)
-
-    connections.push({ close })
-  }
-
-  browser.runtime.onConnect.addListener(onPortConnect)
-
+  // When a new URL is visited, we execute a content script, which creates
+  // a `window.ipfs` object on the page and opens up a new port so that the
+  // `window.ipfs` proxy can talk to us.
   const onTabUpdated = (tabId, changeInfo, tab) => {
     // Some devtools tabs do not have an ID
     if (!tabId || tabId === browser.tabs.TAB_ID_NONE) return
@@ -92,11 +67,49 @@ function createIpfsProxy (getIpfs, getState) {
 
   browser.tabs.onUpdated.addListener(onTabUpdated)
 
+  // Port connection events are emitted when a content script attempts to
+  // communicate with us. Each new URL visited by the user will open a port.
+  // When a port is opened, we create a new IPFS proxy server to listen to the
+  // messages.
+  const onPortConnect = (port) => {
+    if (port.name !== 'ipfs-proxy') return
+
+    const { origin } = new URL(port.sender.url)
+
+    const proxy = createProxyServer(getIpfs, {
+      addListener: (_, handler) => port.onMessage.addListener(handler),
+      removeListener: (_, handler) => port.onMessage.removeListener(handler),
+      postMessage: (data) => port.postMessage(data),
+      getMessageData: (d) => d,
+      pre: ACL_FUNCTIONS.reduce((obj, permission) => {
+        obj[permission] = createAclPreCall(accessControl, origin, permission)
+        return obj
+      }, {})
+    })
+
+    const close = () => {
+      closeProxyServer(proxy)
+      port.onDisconnect.removeListener(onDisconnect)
+      connections = connections.filter(c => c.close !== close)
+    }
+
+    const onDisconnect = () => close()
+
+    // If the port disconnects, clean up the resources for this connection
+    port.onDisconnect.addListener(onDisconnect)
+
+    // Keep track of the open connections so that if destroy is called we can
+    // clean up properly.
+    connections.push({ close })
+  }
+
+  browser.runtime.onConnect.addListener(onPortConnect)
+
   const handle = {
     destroy () {
       connections.forEach(c => c.destroy)
-      browser.runtime.onConnect.removeListener(onPortConnect)
       browser.tabs.onUpdated.removeListener(onTabUpdated)
+      browser.runtime.onConnect.removeListener(onPortConnect)
     }
   }
 
@@ -105,6 +118,9 @@ function createIpfsProxy (getIpfs, getState) {
 
 module.exports = createIpfsProxy
 
+// Creates a "pre" function that is called prior to calling a real function
+// on the IPFS instance. It will throw if access is denied, and ask the user if
+// no access decision has been made yet.
 function createAclPreCall (accessControl, origin, permission) {
   return async (...args) => {
     let access = await accessControl.getAccess(origin, permission)
