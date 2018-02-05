a2b137777719230c15262881e9a51a73eac40040
Alan Shaw <alan@tableflip.io>
2018-02-05 08:45:32 +0000

    adds tests for preAcl

--- add-on/src/lib/ipfs-proxy/pre-acl.js
+++ add-on/src/lib/ipfs-proxy/pre-acl.js
@@ -1,3 +1,5 @@
+const defaultRequestAccess = require('./request-access')
+
 // This are the function that DO NOT require an allow/deny decision by the user.
 // All other IPFS functions require authorization.
 const ACL_WHITELIST = Object.freeze(require('./acl-whitelist.json'))
@@ -5,7 +7,7 @@ const ACL_WHITELIST = Object.freeze(require('./acl-whitelist.json'))
 // Creates a "pre" function that is called prior to calling a real function
 // on the IPFS instance. It will throw if access is denied, and ask the user if
 // no access decision has been made yet.
-function createPreAcl (getState, accessControl, origin, permission) {
+function createPreAcl (getState, accessControl, origin, permission, requestAccess = defaultRequestAccess) {
   return async (...args) => {
     // Check if all access to the IPFS node is disabled
     if (!getState().ipfsProxy) throw new Error('User disabled access to IPFS')
@@ -16,8 +18,8 @@ function createPreAcl (getState, accessControl, origin, permission) {
     let access = await accessControl.getAccess(origin, permission)
 
     if (!access) {
-      const { allow, blanket, remember } = await requestAccess(origin, permission)
-      access = await accessControl.setAccess(origin, blanket ? '*' : permission, allow, remember)
+      const { allow } = await requestAccess(origin, permission)
+      access = await accessControl.setAccess(origin, permission, allow)
     }
 
     if (!access.allow) throw new Error(`User denied access to ${permission}`)
@@ -27,19 +29,3 @@ function createPreAcl (getState, accessControl, origin, permission) {
 }
 
 module.exports = createPreAcl
-
-async function requestAccess (origin, permission) {
-  const msg = `Allow ${origin} to access ipfs.${permission}?`
-
-  // TODO: add checkbox to allow all for this origin
-  let allow
-
-  try {
-    allow = window.confirm(msg)
-  } catch (err) {
-    console.warn('Failed to confirm, possibly not supported in this environment', err)
-    allow = false
-  }
-
-  return { allow, blanket: false }
-}
