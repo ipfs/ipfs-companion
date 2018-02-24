a68d4e627d28ba6ec57b84103b9936730e906d8d
Marcin Rataj <lidel@lidel.org>
2018-02-24 23:55:39 +0100

    Publish update manifest for self-hosted dev build
    ci/firefox/update.json should be exposed under  immutable URL:
    https://ipfs-shipyard.github.io/ipfs-companion/ci/firefox/update.json

--- ci/update-manifest.sh
+++ ci/update-manifest.sh
@@ -4,23 +4,27 @@
 set -e
 
 MANIFEST=add-on/manifest.json
-DEV_BUILD_ADDON_ID="ipfs-companion-dev-build@ci.ipfs.team"
-UPDATE_URL="https://ipfs-shipyard.github.io/ipfs-companion/update.json"
 
-# make it immutable
+# restore original (make this script immutable)
 git checkout $MANIFEST
 
+# Switch to self-hosted dev-build for Firefox (https://github.com/ipfs-shipyard/ipfs-companion/issues/385)
+DEV_BUILD_ADDON_ID="ipfs-companion-dev-build@ci.ipfs.team"
+UPDATE_URL="https://ipfs-shipyard.github.io/ipfs-companion/ci/firefox/update.json"
+
+
+# Use jq for JSON operations
 function set-manifest {
   jq -M --indent 2 "$1" < $MANIFEST > tmp.json && mv tmp.json $MANIFEST
 }
 
-## NAME
+## Set NAME
 # Name includes git revision to make QA and bug reporting easier for users :-)
 REVISION=$(git show-ref --head HEAD | head -c 7)
 set-manifest ".name = \"IPFS Companion (Dev Build @ $REVISION)\""
 grep $REVISION $MANIFEST
 
-## VERSION
+## Set VERSION
 # Browsers do not accept non-numeric values in version string
 # so we calculate some sub-versions based on number of commits in master and  current branch
 COMMITS_IN_MASTER=$(git rev-list --count refs/remotes/origin/master)
@@ -31,11 +35,11 @@ set-manifest ".version = (.version + \".${BUILD_VERSION}\")"
 grep \"version\" $MANIFEST
 
 
-## ADD-ON ID
+## Set ADD-ON ID
 set-manifest ".applications.gecko[\"id\"] = \"$DEV_BUILD_ADDON_ID\""
 grep ipfs-companion-dev-build $MANIFEST
 
-## ADD DEV BUILD UPDATE_URL
+## Add UPDATE_URL
 set-manifest ".applications.gecko[\"update_url\"] = \"$UPDATE_URL\""
 grep update_url $MANIFEST
 
