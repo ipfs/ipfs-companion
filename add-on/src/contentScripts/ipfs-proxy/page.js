9b383f17d42cbe0f57bebfcb0ef02fd33ce665e7
Marcin Rataj <lidel@lidel.org>
2018-04-25 12:11:38 +0200

    Remove API client constructor from window.Ipfs
    Context:
    https://github.com/ipfs-shipyard/ipfs-companion/issues/460#issuecomment-384225346

--- add-on/src/contentScripts/ipfs-proxy/page.js
+++ add-on/src/contentScripts/ipfs-proxy/page.js
@@ -1,9 +1,7 @@
 'use strict'
 
-const Ipfs = require('ipfs')
 const { createProxyClient } = require('ipfs-postmsg-proxy')
 const _Buffer = Buffer
 
 window.Buffer = window.Buffer || _Buffer
-window.Ipfs = window.Ipfs || Ipfs
 window.ipfs = window.ipfs || createProxyClient()
