0ea698e2db32b07b823ebf9edb40a55307321fb7
Marcin Rataj <lidel@lidel.org>
2018-08-10 13:16:03 +0200

    refactor(libdweb): reuse dir listing from js-ipfs

--- add-on/src/lib/ipfs-protocol-libdweb.js
+++ add-on/src/lib/ipfs-protocol-libdweb.js
@@ -3,10 +3,11 @@
 
 import {asyncIterateStream} from 'async-iterate-stream/asyncIterateStream'
 
+const { resolver } = require('ipfs-http-response')
 const { mimeSniff } = require('./mime-sniff')
 const toArrayBuffer = require('to-arraybuffer')
 const peek = require('buffer-peek-stream')
-const dirView = require('./dir-view')
+// const dirView = require('./dir-view')
 const PathUtils = require('ipfs/src/http/gateway/utils/path')
 const isStream = require('is-stream')
 
@@ -92,65 +93,43 @@ function immutableIpfsPath (url, dnsLink) {
 }
 
 async function getResponse (ipfs, url, path) {
-  // We're using ipfs.ls to figure out if a path is a file or a directory.
-  //
-  // If the listing is empty then it's (likely) a file
-  // If the listing has 1 entry then it's a directory
-  // If the listing has > 1 entry && all the paths are the same then directory
-  // else file
-  //
-  // It's not pretty, but the alternative is to use the object or dag API's
-  // and inspect the data returned by them to see if it's a file or dir.
-  //
-  // Right now we can't use either of these because:
-  // 1. js-ipfs object API does not take paths (only cid)
-  // 2. js-ipfs-api does not support dag API at all
-  //
-  // The second alternative would be to resolve the path ourselves using the
-  // object API, but that could take a while for long paths.
-  const listing = await ipfs.ls(path)
-
-  if (isDirectory(listing)) {
-    return getDirectoryListingOrIndexResponse(ipfs, url, path, listing)
+  let cid
+  try {
+    // try direct resolver
+    const dag = await resolver.cid(ipfs, path)
+    // console.log('resolver.cid: ', dag)
+    cid = dag.cid
+  } catch (err) {
+    const errorToString = err.toString()
+    // handle directories
+    if (errorToString === 'Error: This dag node is a directory') {
+      const dirCid = err.cid
+      const data = await resolver.directory(ipfs, url, dirCid)
+      // console.log('resolver.directory', data)
+      // TODO: redirect so directory always end with `/`
+      if (typeof data === 'string') {
+        // return HTML with directory listing
+        return new TextEncoder('utf-8').encode(data).buffer
+      } else if (Array.isArray(data)) {
+        // return first index file
+        path = PathUtils.joinURLParts(path, data[0].name)
+        return getResponse(ipfs, url, path)
+      }
+      throw new Error('Invalid output of resolver.directory')
+    }
+    throw err
   }
 
   // Efficient mime-sniff over initial bytes
   // TODO: rignt now it is only logged, use contentType somehow
   const { stream, contentType } = await new Promise((resolve, reject) => {
-    peek(ipfs.files.catReadableStream(path), 512, (err, data, stream) => {
+    peek(ipfs.files.catReadableStream(cid), 512, (err, data, stream) => {
       if (err) return reject(err)
-      const contentType = mimeSniff(data, path) || 'text/plain'
+      const contentType = mimeSniff(data, url) || 'application/octet-stream'
       resolve({ stream, contentType })
     })
   })
   console.log(`[ipfs-companion] [ipfs://] handler read ${path} and internally mime-sniffed it as ${contentType}`)
   //
   return stream
-}
-
-function isDirectory (listing) {
-  if (!listing.length) return false
-  if (listing.length === 1) return true
-  // If every path in the listing is the same, IPFS has listed blocks for a file
-  // if not then it is a directory listing.
-  const path = listing[0].path
-  return !listing.every(f => f.path === path)
-}
-
-function getDirectoryListingOrIndexResponse (ipfs, url, path, listing) {
-  const indexFileNames = ['index', 'index.html', 'index.htm']
-  const index = listing.find((l) => indexFileNames.includes(l.name))
-
-  if (index) {
-    /* TODO: pass mime-type to libdweb somehow?
-    let contentType = 'text/plain'
-    if (index.name.endsWith('.html') || index.name.endsWith('.htm')) {
-      contentType = 'text/html'
-    }
-    */
-    return ipfs.files.catReadableStream(PathUtils.joinURLParts(path, index.name))
-  }
-
-  const response = dirView.render(url, listing)
-  return new TextEncoder('utf-8').encode(response).buffer
 }
