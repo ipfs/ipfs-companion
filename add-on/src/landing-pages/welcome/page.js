2a678748867f55c56952ddd5b72a3aab35d1bcb2
Diogo Silva <fsdiogo@gmail.com>
2018-09-06 15:50:18 +0100

    wip: small refactor

--- add-on/src/landing-pages/welcome/page.js
+++ add-on/src/landing-pages/welcome/page.js
@@ -33,20 +33,27 @@ function createWelcomePage (i18n) {
   }
 }
 
-const renderResources = () => html`
-  <div class="w-80 navy f5">
-    <p class="aqua mb1">Want to help?</p>
-    <p class="mt0 mb4 lh-copy">Join the <a class="navy link underline-under hover-aqua" href="https://github.com/ipfs/community/" target="_blank">IPFS Community</a>!</p>
+const renderResources = () => {
+  const labelClass = 'aqua mb1'
+  const copyClass = 'mt0 mb4 lh-copy'
+  const anchorClass = 'navy link underline-under hover-aqua'
 
-    <p class="aqua mb1">New to IPFS?</p>
-    <p class="mt0 mb4 lh-copy">Read the <a class="navy link underline-under hover-aqua" href="http://docs.ipfs.io/" target="_blank">documentation</a> to learn about the basic <a class="navy link underline-under hover-aqua" href="http://docs.ipfs.io/guides/concepts" target="_blank">concepts</a> and working with IPFS.</p>
+  return html`
+    <div class="w-80 navy f5">
+      <p class="${labelClass}">Want to help?</p>
+      <p class="${copyClass}">Join the <a class="${anchorClass}" href="https://github.com/ipfs/community/" target="_blank">IPFS Community</a>!</p>
 
-    <p class="aqua mb1">Got questions?</p>
-    <p class="mv0 lh-copy">Visit the <a class="navy link underline-under hover-aqua" href="https://discuss.ipfs.io/" target="_blank">Discussion and Support Forum</a>.</p>
-  </div>
-`
+      <p class="${labelClass}">New to IPFS?</p>
+      <p class="${copyClass}">Read the <a class="${anchorClass}" href="http://docs.ipfs.io/" target="_blank">documentation</a> to learn about the basic <a class="${anchorClass}" href="http://docs.ipfs.io/guides/concepts" target="_blank">concepts</a> and working with IPFS.</p>
+
+      <p class="${labelClass}">Got questions?</p>
+      <p class="${copyClass} mv0">Visit the <a class="${anchorClass}" href="https://discuss.ipfs.io/" target="_blank">Discussion and Support Forum</a>.</p>
+    </div>
+  `
+}
 
 const renderVideos = () => {
+  const anchorClass = 'relative overflow-hidden br2 o-90 glow'
   const videoWidth = 240
   const videoHeight = 180
 
@@ -69,7 +76,7 @@ const renderVideos = () => {
     <div class="w-80 flex justify-between-ns aqua f5">
       <div class="flex flex-column">
         <p>IPFS Alpha Demo</p>
-        <a class="relative overflow-hidden br2 o-90 glow" style="height: ${videoHeight}px" href="https://www.youtube.com/watch?feature=player_embedded&v=8CMxDNuuAiQ" target="_blank">
+        <a class="${anchorClass}" style="height: ${videoHeight}px" href="https://www.youtube.com/watch?feature=player_embedded&v=8CMxDNuuAiQ" target="_blank">
           <img width="${videoWidth}" height="${videoHeight}" src="https://img.youtube.com/vi/8CMxDNuuAiQ/0.jpg" alt="IPFS Alpha Demo" />
           ${overlayDiv()}
           ${playSvg()}
@@ -78,7 +85,7 @@ const renderVideos = () => {
 
       <div class="flex flex-column">
         <p>IPFS and the Permanent Web</p>
-        <a class="relative overflow-hidden br2 o-90 glow" style="height: ${videoHeight}px" href="https://www.youtube.com/watch?feature=player_embedded&v=HUVmypx9HGI" target="_blank">
+        <a class="${anchorClass}" style="height: ${videoHeight}px" href="https://www.youtube.com/watch?feature=player_embedded&v=HUVmypx9HGI" target="_blank">
           <img width="${videoWidth}" height="${videoHeight}" src="https://img.youtube.com/vi/HUVmypx9HGI/0.jpg" alt="IPFS and the Permanent Web" />
           ${overlayDiv()}
           ${playSvg()}
@@ -89,6 +96,7 @@ const renderVideos = () => {
 }
 
 const renderProjects = () => {
+  const anchorClass = 'flex flex-column items-center navy link hover-aqua'
   const logoWidth = 80
 
   return html`
@@ -96,17 +104,17 @@ const renderProjects = () => {
       <p class="mb4 aqua f5">Related Projects</p>
 
       <div class="flex justify-between-ns">
-        <a class="flex flex-column items-center navy link hover-aqua" href="https://libp2p.io/" target="_blank">
+        <a class="${anchorClass}" href="https://libp2p.io/" target="_blank">
           <img width="${logoWidth}" src="${libp2pLogo}" alt="libp2p Logo">
           <p>libp2p.io</p>
         </a>
 
-        <a class="flex flex-column items-center navy link hover-aqua" href="https://multiformats.io/" target="_blank">
+        <a class="${anchorClass}" href="https://multiformats.io/" target="_blank">
           <img width="${logoWidth}" src="${multiformatsLogo}" alt="Multiformats Logo">
           <p>multiformats.io</p>
         </a>
 
-        <a class="flex flex-column items-center navy link hover-aqua" href="https://ipld.io/" target="_blank">
+        <a class="${anchorClass}" href="https://ipld.io/" target="_blank">
           <img width="${logoWidth}" src="${ipldLogo}" alt="IPLD Logo">
           <p>ipld.io</p>
         </a>
