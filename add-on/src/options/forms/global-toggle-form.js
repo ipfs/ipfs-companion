602769e8ed7e7adfe57c1c59c131f967390e926c
Marcin Rataj <lidel@lidel.org>
2018-09-19 12:25:15 +0200

    fix: checkbox for panel_headerActiveToggleTitle

--- add-on/src/options/forms/global-toggle-form.js
+++ add-on/src/options/forms/global-toggle-form.js
@@ -5,11 +5,11 @@ const browser = require('webextension-polyfill')
 const html = require('choo/html')
 
 function globalToggleForm ({ active, onOptionChange }) {
-  const toggle = onOptionChange('active')
+  const toggle = onOptionChange('active', checked => !checked)
   return html`
     <form class="dib mb3">
-      <label for="active" class="dib pa3 pointer ${!active ? 'bg-aqua-muted br2' : ''}">
-        <input class="mr2 pointer" id="active" type="checkbox" onchange=${toggle} checked=${active} />
+      <label for="active" class="dib pa3 pointer ${!active ? 'charcoal bg-gray-muted br2' : ''}">
+        <input class="mr2 pointer" id="active" type="checkbox" onchange=${toggle} checked=${!active} />
         ${browser.i18n.getMessage('panel_headerActiveToggleTitle')}
       </label>
     </form>
