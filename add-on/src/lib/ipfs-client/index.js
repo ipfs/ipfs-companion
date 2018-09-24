aa616fd07c4537937bab9b9901f87ae148c39462
Marcin Rataj <lidel@lidel.org>
2018-09-24 19:31:22 +0200

    fix: reload bundled WebUI on IPFS Client change
    WebUI would notice issue eventually, but this gives user an instant
    fix and results in better UX.

--- add-on/src/lib/ipfs-client/index.js
+++ add-on/src/lib/ipfs-client/index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const browser = require('webextension-polyfill')
 const external = require('./external')
 const embedded = require('./embedded')
 
@@ -14,7 +15,9 @@ async function initIpfsClient (opts) {
     client = external
   }
 
-  return client.init(opts)
+  const instance = await client.init(opts)
+  await _reloadIpfsClientDependents()
+  return instance
 }
 
 async function destroyIpfsClient () {
@@ -23,6 +26,22 @@ async function destroyIpfsClient () {
       await client.destroy()
     } finally {
       client = null
+      await _reloadIpfsClientDependents()
+    }
+  }
+}
+
+async function _reloadIpfsClientDependents () {
+  if (browser.tabs && browser.tabs.query) {
+    const tabs = await browser.tabs.query({})
+    if (tabs) {
+      tabs.forEach((tab) => {
+        // detect bundled webui in any of open tabs
+        if (!tab.url.startsWith('http') && tab.url.includes('/webui/index.html#/')) {
+          browser.tabs.reload(tab.id)
+          console.log('[ipfs-companion] reloading bundled webui')
+        }
+      })
     }
   }
 }
