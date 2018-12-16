b633eb467202f2adfae39a15380d4242bca1987b
Marcin Rataj <lidel@lidel.org>
2018-12-17 00:16:18 +0100

    feat(window.ipfs): opt-in ipfsx experiment
    This adds an opt-in ipfsx experiment.
    In short, if `experiments:{ipfsx:true}` is passed `window.ipfs.enable`
    will return IPFS API instance wrapped in ipfsx prototype from
    https://github.com/ipfs-shipyard/ipfsx
    
    ```
    let ipfs = await window.ipfs.enable({
        commands: ['add','files.addPullStream'],
        experiments: { ipfsx: true }
    })
    ```

--- add-on/src/contentScripts/ipfs-proxy/page.js
+++ add-on/src/contentScripts/ipfs-proxy/page.js
@@ -1,36 +1,37 @@
 'use strict'
 
 const _Buffer = Buffer
+const { assign, freeze } = Object
 
 // TODO: (wip) this should not be injected by default into every page,
 // instead should be lazy-loaded when .enable() method is called for the first time
 const { createProxyClient } = require('ipfs-postmsg-proxy')
-const { call } = require('postmsg-rpc')
 
 function createEnableCommand (proxyClient) {
   return {
     enable: async (opts) => {
+      // (This should be a lazy-load)
       // Send message to proxy server for additional validation
       // eg. trigger user prompt if a list of requested capabilities is not empty
       // or fail fast and throw if IPFS Proxy is disabled globally
-      await call('proxy.enable', opts)
+      await require('postmsg-rpc').call('proxy.enable', opts)
       // Additional client-side features
-      if (opts) {
-        if (opts.ipfsx === true) {
-          // TODO: wrap API in https://github.com/alanshaw/ipfsx
-          // return ipfsx(proxyClient)
+      if (opts && opts.experiments) {
+        if (opts.experiments.ipfsx) {
+          // Experiment: wrap API with  https://github.com/alanshaw/ipfsx
+          return freeze(require('ipfsx')(proxyClient))
         }
       }
-      return Object.freeze(proxyClient)
+      return freeze(proxyClient)
     }
   }
 }
 
 function createWindowIpfs () {
   const proxyClient = createProxyClient()
-  Object.assign(proxyClient, createEnableCommand(proxyClient))
+  assign(proxyClient, createEnableCommand(proxyClient))
   // TODO: return thin object with lazy-init inside of window.ipfs.enable
-  return Object.freeze(proxyClient)
+  return freeze(proxyClient)
 }
 
 // TODO: we should remove Buffer and add support for Uint8Array/ArrayBuffer natively
