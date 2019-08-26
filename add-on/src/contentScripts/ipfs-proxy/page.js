d4d8806e6e37b8a9dfe12eba4078f600c7ceabd2
Marcin Rataj <lidel@lidel.org>
2019-08-26 16:18:27 +0200

    chore: updated deprecation of window.ipfs

--- add-on/src/contentScripts/ipfs-proxy/page.js
+++ add-on/src/contentScripts/ipfs-proxy/page.js
@@ -35,7 +35,7 @@ function createWindowIpfs () {
   for (const cmd in proxyClient) {
     const fn = proxyClient[cmd]
     proxyClient[cmd] = function () {
-      console.warn('Calling commands directly on window.ipfs is deprecated and will be removed on 2019-09-01. Use API instance returned by window.ipfs.enable() instead. More: https://github.com/ipfs-shipyard/ipfs-companion/blob/master/docs/window.ipfs.md')
+      console.warn('Calling commands directly on window.ipfs is deprecated and will be removed in the future. To future-proof your app use API instance returned by window.ipfs.enable() instead. Current best practices can be found at: https://github.com/ipfs-shipyard/ipfs-companion/blob/master/docs/window.ipfs.md')
       return fn.apply(this, arguments)
     }
   }
