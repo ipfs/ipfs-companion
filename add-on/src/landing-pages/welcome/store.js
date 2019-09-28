6154fd65889f2811122755b18ad10c02478bd817
colinfruit <17092461+colinfruit@users.noreply.github.com>
2019-09-27 23:42:44 -0400

    add 'Open Web UI' button to the welcome page

--- add-on/src/landing-pages/welcome/store.js
+++ add-on/src/landing-pages/welcome/store.js
@@ -1,25 +1,35 @@
 'use strict'
 /* eslint-env browser, webextensions */
-
 function createWelcomePageStore (i18n, runtime) {
   return function welcomePageStore (state, emitter) {
     state.isIpfsOnline = null
     state.peerCount = null
+    state.webuiRootUrl = null
     let port
     emitter.on('DOMContentLoaded', async () => {
       emitter.emit('render')
       port = runtime.connect({ name: 'browser-action-port' })
       port.onMessage.addListener(async (message) => {
         if (message.statusUpdate) {
+          const webuiRootUrl = message.statusUpdate.webuiRootUrl
           const peerCount = message.statusUpdate.peerCount
           const isIpfsOnline = peerCount > -1
-          if (isIpfsOnline !== state.isIpfsOnline || peerCount !== state.peerCount) {
+          if (isIpfsOnline !== state.isIpfsOnline || peerCount !== state.peerCount || webuiRootUrl !== state.webuiRootUrl) {
+            state.webuiRootUrl = webuiRootUrl
             state.isIpfsOnline = isIpfsOnline
             state.peerCount = peerCount
             emitter.emit('render')
           }
         }
       })
+    })
+
+    emitter.on('openWebUi', async () => {
+      try {
+        browser.tabs.create({ url: state.webuiRootUrl })
+      } catch (error) {
+        console.error(`Unable Open Web UI due to ${error}`)
+      }
     })
   }
 }
