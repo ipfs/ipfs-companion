95c96dacc4dd2f9c7cb790d856c1bb979b3d8ee3
Jessica Schilling <jessica@protocol.ai>
2020-07-02 15:07:47 -0600

    Revert "Two buttons: share files, go to my node"
    This reverts commit 931cd00534df0146637e2c8d06ed84ff8d144a76.

--- add-on/src/popup/browser-action/tools.js
+++ add-on/src/popup/browser-action/tools.js
@@ -6,7 +6,6 @@ const html = require('choo/html')
 const toolsButton = require('./tools-button')
 const toolsItem = require('./tools-item')
 
-
 module.exports = function tools ({
   active,
   redirect,
@@ -22,7 +21,7 @@ module.exports = function tools ({
   const activeRedirectSwitch = active && ipfsNodeType !== 'embedded'
 
   return html`
-    <div class="bg-aqua-muted pv1 br bl bb bw1 b--white flex">
+    <div class="bg-aqua-muted pv1 ph1 br bl bb bw1 b--white flex">
       ${toolsButton({
         text: browser.i18n.getMessage('panel_quickImport'),
         disabled: !activeQuickImport,
@@ -33,15 +32,13 @@ module.exports = function tools ({
         disabled: !activeWebUI,
         onClick: onOpenWebUi
       })}
-    </div>
-    <div>
-    ${toolsItem({
-      text: browser.i18n.getMessage('panel_redirectToggle'),
-      title: browser.i18n.getMessage('panel_redirectToggleTooltip'),
-      disabled: !activeRedirectSwitch,
-      switchValue: redirect && activeRedirectSwitch,
-      onClick: onToggleGlobalRedirect
-    })}
+      ${toolsItem({
+        text: browser.i18n.getMessage('panel_redirectToggle'),
+        title: browser.i18n.getMessage('panel_redirectToggleTooltip'),
+        disabled: !activeRedirectSwitch,
+        switchValue: redirect && activeRedirectSwitch,
+        onClick: onToggleGlobalRedirect
+      })}
     </div>
   `
 }
