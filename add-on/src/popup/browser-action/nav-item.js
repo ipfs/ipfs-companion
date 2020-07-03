57be2460b6947d0c4a6026707cf1181f3febcecf
Jessica Schilling <jessica@protocol.ai>
2020-07-03 16:06:55 -0600

    Update nav-item.js
    Affordance for copy menu-item subtitles, not dynamic yet

--- add-on/src/popup/browser-action/nav-item.js
+++ add-on/src/popup/browser-action/nav-item.js
@@ -4,7 +4,7 @@
 const html = require('choo/html')
 const switchToggle = require('../../pages/components/switch-toggle')
 
-function navItem ({ icon, text, title, disabled, style, onClick, switchValue }) {
+function navItem ({ icon, text, title, subtitle, disabled, style, onClick, switchValue }) {
   let buttonStyle = 'black button-reset db w-100 bg-white b--none outline-0--focus pv2 ph3 f6 tl'
   if (disabled) {
     buttonStyle += ' o-40'
@@ -20,9 +20,10 @@ function navItem ({ icon, text, title, disabled, style, onClick, switchValue }) 
 
   return html`
 
-    <button class="flex flex-row items-center justify-between ${buttonStyle}"
+    <button class="${buttonStyle}"
             onclick=${disabled ? null : onClick}  title="${title || ''}" ${disabled ? 'disabled' : ''}>
-      <div class="truncate">${text}</div>${switchToggle({ checked: switchValue, disabled, style: 'fr ml2' })}
+      <div class="flex flex-row items-center justify-between"><div class="truncate">${text}</div>${switchToggle({ checked: switchValue, disabled, style: 'fr ml2' })}</div>
+      <div class="f7 o-40 truncate mt1">bafybeieltqren5gbivmavam532nvlsyxy725bpxqk77zvmaflrdyvu4yxm</div>
     </button>
   `
 }
