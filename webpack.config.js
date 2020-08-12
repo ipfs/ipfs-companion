1edea91fa1177cfb62b797772d79a0ab8a06e11e
Marcin Rataj <lidel@lidel.org>
2020-08-12 15:38:15 +0200

    chore: fixing @hapi/joi error

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
-const merge = require('webpack-merge')
+const { merge } = require('webpack-merge')
 const SimpleProgressWebpackPlugin = require('simple-progress-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
