7ae89e6eaed8bfd2eaed2c7c6526a9115cb6b46f
Marcin Rataj <lidel@lidel.org>
2020-09-29 20:25:19 +0200

    fix(js-ipfs): enable webrtc for js-ipfs
    this enables webrtc-based signaling and delegates for experimental
    js-ipfs running in extension context (non-Brave) when no custom config
    is provided

--- add-on/src/lib/ipfs-client/embedded.js
+++ add-on/src/lib/ipfs-client/embedded.js
@@ -15,6 +15,22 @@ exports.init = async function init (opts) {
   const defaultOpts = JSON.parse(optionDefaults.ipfsNodeConfig)
   const userOpts = JSON.parse(opts.ipfsNodeConfig)
   const ipfsOpts = mergeOptions(defaultOpts, userOpts, { start: true })
+  const missing = (array) => (!Array.isArray(array) || !array.length)
+  const { Addresses } = ipfsOpts.config
+  if (missing(Addresses.Swarm)) {
+    Addresses.Swarm = [
+      '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star',
+      '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star'
+    ]
+  }
+  if (missing(ipfsOpts.Delegates)) {
+    Addresses.Delegates = [
+      '/dns4/node0.delegate.ipfs.io/tcp/443/https',
+      '/dns4/node1.delegate.ipfs.io/tcp/443/https',
+      '/dns4/node2.delegate.ipfs.io/tcp/443/https',
+      '/dns4/node3.delegate.ipfs.io/tcp/443/https'
+    ]
+  }
   node = await Ipfs.create(ipfsOpts)
   return node
 }
