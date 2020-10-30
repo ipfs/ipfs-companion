882dc0c9fee9f48ff971f5a6a8fc2e3bfe5f7bb1
Jessica Schilling <jessica@protocol.ai>
2020-10-29 18:18:30 -0600

    Button and language tweaks

--- add-on/src/popup/browser-action/tools-button.js
+++ add-on/src/popup/browser-action/tools-button.js
@@ -4,7 +4,7 @@
 const html = require('choo/html')
 
 function toolsButton ({ iconD, iconSize, text, title, disabled, style, onClick }) {
-  let buttonStyle = 'button-reset fade-in w-50 ba bw1 snow b--snow bg-transparent f7 ph1 pv0 br4 ma1 flex justify-center items-center truncate'
+  let buttonStyle = 'fade-in w-50 ba bw1 snow b--snow bg-transparent f7 ph1 pv0 br4 ma1 flex justify-center items-center truncate'
   if (disabled) {
     buttonStyle += ' o-60'
   } else {
@@ -19,10 +19,10 @@ function toolsButton ({ iconD, iconSize, text, title, disabled, style, onClick }
 
   return html`
 
-    <button class="${buttonStyle}" onclick=${disabled ? null : onClick}  title="${title || ''}" ${disabled ? 'disabled' : ''}>
+    <div class="${buttonStyle}" onclick=${disabled ? null : onClick}  title="${title || ''}" ${disabled ? 'disabled' : ''}>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" class="mr1" width="${iconSize}" height="${iconSize}"><path fill="currentColor" d="${iconD}"/></svg>
       <div class="flex flex-row items-center justify-between"><div class="truncate">${text}</div></div>
-    </button>
+    </div>
   `
 }
 
