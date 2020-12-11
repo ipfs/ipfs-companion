c574d2e5b00165bfdc890d4762e9fddd974801e1
Marcin Rataj <lidel@lidel.org>
2020-12-11 16:45:16 +0100

    fix: URI router preserves path
    - we preserve path, so URIs like below work:
      #ipfs%3A%2F%2FQmXoypizjW3WknFiJnKLwHCnL72vedxjQkDDP1mXWo6uco%2Fwiki%2FDiego_Maradona.html
    - hide error UI and display "Processing IPFS request.." while
      redirecting for a nicer UX (no error flash)
    
    TODO: add support for dnslink names in ipns namespace

--- add-on/src/lib/ipfs-request.js
+++ add-on/src/lib/ipfs-request.js
@@ -508,7 +508,7 @@ function isSafeToRedirect (request, runtime) {
 
 // This is just a placeholder that we had to provide -- removed in normalizedRedirectingProtocolRequest()
 // It has to match URL from manifest.json/protocol_handlers
-const redirectingProtocolEndpoint = 'https://gateway.ipfs.io/ipfs/QmVGC4uCBDVEhCzsaJmvR5nVDgChM97kcYNehVm7L9jxtc#'
+const redirectingProtocolEndpoint = 'https://gateway.ipfs.io/ipfs/bafkreihnpo5zt7qt3lvmk4zk2opran7ybbe3gfp5ruqf5iirdyxaxqvwtu#'
 
 function redirectingProtocolRequest (request) {
   return request.url.startsWith(redirectingProtocolEndpoint)
