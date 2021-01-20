138b24898c9da25fa6b12a8cb54852a6cdfe4112
Marcin Rataj <lidel@lidel.org>
2021-01-20 03:19:03 +0100

    feat: brave node indicator
    This adds brave badge to indicate that companion is backed by the node
    managed by the brave browser: https://brave.com/ipfs-support/

--- add-on/src/popup/logo.js
+++ add-on/src/popup/logo.js
@@ -2,17 +2,23 @@
 /* eslint-env browser, webextensions */
 
 const html = require('choo/html')
+const { braveNodeType } = require('../lib/ipfs-client/brave')
 
 function logo ({ path, size = 52, ipfsNodeType = 'external', isIpfsOnline = true, heartbeat = true }) {
-  const logoTypePrefix = ipfsNodeType.startsWith('embedded') ? 'js-' : ''
-  const logoFileName = `${logoTypePrefix}ipfs-logo-${isIpfsOnline ? 'on' : 'off'}.svg`
   return html`
     <img
       alt="IPFS"
-      src="${path}/${logoFileName}"
+      src="${path}/${logoFileName(ipfsNodeType, isIpfsOnline)}"
       class="v-mid ${isIpfsOnline ? '' : 'o-40'} ${isIpfsOnline && heartbeat ? 'heartbeat' : ''}"
       style="width:${size}px; height:${size}px" />
   `
+}
+
+function logoFileName (nodeType, isIpfsOnline) {
+  let prefix
+  if (nodeType.startsWith('embedded')) prefix = 'js-'
+  if (nodeType === braveNodeType) prefix = 'brave-'
+  return `${prefix || ''}ipfs-logo-${isIpfsOnline ? 'on' : 'off'}.svg`
 }
 
 module.exports = logo
