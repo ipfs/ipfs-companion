8d94114f18652a8fd6f119c90f774fc27fe2763e
Marcin Rataj <lidel@lidel.org>
2021-01-20 21:32:20 +0100

    feat: default to brave node
    This is a small change that will default to endpoint provided by Brave
    if the current endpoint (by default 127.0.0.1:8080) is offline.
    
    This means user won't need to go to preferences and switch the backend
    to "Provided by Brave", new users will have that set for them
    automatically.
    
    It also fixes a bug where "Welcome" page did not update when enabling
    Brave node required user interaction – we now reload internal pages
    when a new backend finished its init chores.

--- add-on/src/lib/on-installed.js
+++ add-on/src/lib/on-installed.js
@@ -10,24 +10,48 @@ exports.updatePage = 'https://github.com/ipfs-shipyard/ipfs-companion/releases/t
 exports.onInstalled = async (details) => {
   // details.temporary === run via `npm run firefox`
   if (details.reason === 'install' || details.temporary) {
-    await browser.storage.local.set({ showLandingPage: 'onInstallWelcome' })
+    await browser.storage.local.set({ onInstallTasks: 'onFirstInstall' })
   } else if (details.reason === 'update' || details.temporary) {
-    await browser.storage.local.set({ showLandingPage: 'onVersionUpdate' })
+    await browser.storage.local.set({ onInstallTasks: 'onVersionUpdate' })
   }
 }
 
-exports.showPendingLandingPages = async () => {
-  const { showLandingPage, displayReleaseNotes } = await browser.storage.local.get(['showLandingPage', 'displayReleaseNotes'])
-  switch (showLandingPage) {
-    case 'onInstallWelcome':
-      await browser.storage.local.remove('showLandingPage')
+exports.runPendingOnInstallTasks = async () => {
+  const { onInstallTasks, displayReleaseNotes } = await browser.storage.local.get(['onInstallTasks', 'displayReleaseNotes'])
+  await browser.storage.local.remove('onInstallTasks')
+  switch (onInstallTasks) {
+    case 'onFirstInstall':
+      await useNativeNodeIfFeasible(browser)
       return browser.tabs.create({
         url: exports.welcomePage
       })
     case 'onVersionUpdate':
-      await browser.storage.local.remove('showLandingPage')
       if (!displayReleaseNotes) return
       await browser.storage.local.set({ dismissedUpdate: version })
       return browser.tabs.create({ url: exports.updatePage + version })
+  }
+}
+
+async function useNativeNodeIfFeasible (browser) {
+  // lazy-loaded dependencies due to debug package
+  // depending on the value of localStorage.debug, which is set later
+  const debug = require('debug')
+  const log = debug('ipfs-companion:on-installed')
+  log.error = debug('ipfs-companion:on-installed:error')
+  const { brave, braveNodeType } = require('./ipfs-client/brave')
+  const { ipfsNodeType, ipfsApiUrl } = await browser.storage.local.get(['ipfsNodeType', 'ipfsApiUrl'])
+
+  // Brave >= v1.19 (https://brave.com/ipfs-support/)
+  if (typeof brave !== 'undefined' && ipfsNodeType !== braveNodeType) {
+    try {
+      log(`brave detected, but node type is ${ipfsNodeType}. testing external endpoint at ${ipfsApiUrl}`)
+      const response = await (await fetch(`${ipfsApiUrl}/api/v0/id`, { method: 'post' })).json()
+      if (typeof response.ID === 'undefined') throw new Error(`unable to read PeerID from API at ${ipfsApiUrl}`)
+      log(`endpoint is online, PeerID is ${response.ID}, nothing to do`)
+    } catch (e) {
+      log.error(`endpoint ${ipfsApiUrl} does not work`, e)
+      log('switching node type to one provided by brave')
+      await browser.storage.local.set({ ipfsNodeType: braveNodeType })
+    }
   }
 }
