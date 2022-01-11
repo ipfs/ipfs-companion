69d1d939b3f536d8531fa08b4e083d3341b4f153
David Justice <meandave@protonmail.com>
2022-01-10 20:07:40 -0500

    feat: Add mv3 directory and boilerplate extension
    - adds mv3/ directory
    - setup fast builds with esbuild
    - setup prettier for formatting
    - setup eslint
    - setup typescript
    - adds BASIC handling of ipfs.io & dweb.link URLs to local node via
    declarativeNetRequest api (manifest v3)
    - adds BASIC handling of DNSLink requests by parsing, talking to local
    node via ipfs-http-client, and adding rule to declarativeNetRequest
    ruleset
    - adds boilerplate for popup

---
+++ mv3/addon/background.ts
@@ -0,0 +1,92 @@
+import browser, { Runtime } from "webextension-polyfill";
+
+import { create } from "ipfs-http-client";
+import IsIpfs from "is-ipfs";
+const check = String.fromCodePoint(0x2714);
+
+let lifeline: Runtime.Port;
+const client = create({ url: "http://localhost:5001/api/v0" });
+
+browser.runtime.onConnect.addListener((port: Runtime.Port) => {
+  if (port.name === "keepAlive") {
+    lifeline = port;
+    setTimeout(keepAliveForced, 295e3); // 5 minutes minus 5 seconds
+    port.onDisconnect.addListener(keepAliveForced);
+  }
+});
+
+function keepAliveForced() {
+  if (lifeline) lifeline.disconnect();
+  lifeline = null;
+  keepAlive();
+}
+
+async function keepAlive() {
+  if (lifeline) return;
+  for (const tab of await browser.tabs.query({ url: "*://*/*" })) {
+    try {
+      await browser.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: () => browser.runtime.connect({ name: "keepAlive" }),
+      });
+      browser.tabs.onUpdated.removeListener(retryOnTabUpdate);
+      return;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  browser.tabs.onUpdated.addListener(retryOnTabUpdate);
+}
+
+function isNewTabURL(url) {
+  if (!url) return true;
+  return url.includes("chrome://");
+}
+
+async function resolveDNSAndAddRule(domain) {
+  const cid = await client.dns(domain);
+  if (!cid) return;
+
+  const id = Math.floor(Math.random() * 29999);
+  // @ts-ignore
+  browser.declarativeNetRequest.updateDynamicRules(
+    {
+      addRules: [
+        {
+          id: id,
+          priority: 1,
+          action: {
+            type: "redirect",
+            redirect: { url: `https://ipfs.io/${cid}` },
+          },
+          condition: { urlFilter: domain, resourceTypes: ["main_frame"] },
+        },
+      ],
+    },
+    () => {
+      console.log(
+        `Added rule ${id}, cid: ${id}, domain: ${domain}. Will redirect to ipfs gateway on next page load ${check}`
+      );
+    }
+  );
+}
+
+async function clearDynamicRules() {
+  // @ts-ignore
+  browser.declarativeNetRequest.getDynamicRules((rules) => {
+    // @ts-ignore
+    browser.declarativeNetRequest.updateDynamicRules({
+      removeRuleIds: rules.map((r) => r.id),
+    });
+  });
+}
+
+async function retryOnTabUpdate(tabId, info) {
+  if (isNewTabURL(info.url) || IsIpfs.url(info.url)) return;
+
+  const currentUrl = new URL(info.url);
+  await resolveDNSAndAddRule(currentUrl.host);
+}
+
+clearDynamicRules();
+keepAlive();
