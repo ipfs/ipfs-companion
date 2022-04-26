6af06a8c768d2c777c691ce3f55ddd5fdc0b7664
Jisu Kim <webmaster@alien.moe>
2022-04-27 03:51:18 +0900

    support basic auth

--- add-on/src/lib/ipfs-client/external.js
+++ add-on/src/lib/ipfs-client/external.js
@@ -9,7 +9,17 @@ const httpClient = require('ipfs-http-client')
 
 exports.init = async function (browser, opts) {
   log(`init with IPFS API at ${opts.apiURLString}`)
-  const clientConfig = opts.apiURLString
+  const url = new URL(opts.apiURLString)
+  const clientConfig = (url.username || url.password)
+    ? {
+        host: url.host,
+        port: url.port,
+        protocol: url.protocol,
+        headers: {
+          authorization: 'Basic ' + btoa(url.username + ':' + url.password)
+        }
+      }
+    : url
   // https://github.com/ipfs/js-ipfs/tree/master/packages/ipfs-http-client#importing-the-module-and-usage
   const api = httpClient(clientConfig)
   return api
