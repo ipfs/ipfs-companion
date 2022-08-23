3c6c2af374a7d6921337d1fc7c7475427aa98d8c
Nishant Arora <me@nishantarora.in>
2022-08-23 14:31:26 -0600

    Adding more cases for offline detection

--- add-on/src/lib/ipfs-client/reloaders/localGatewayReloader.js
+++ add-on/src/lib/ipfs-client/reloaders/localGatewayReloader.js
@@ -22,8 +22,17 @@ class LocalGatewayReloader extends ReloaderBase {
   }
 
   validation ({ url, title }) {
-    // Check if the url is the local gateway url and if url is the same is title, it never got loaded.
-    return url.startsWith(this.customGatewayUrl) && (url === title)
+    // Check if the url is the local gateway url and if the title is contained within the url then it was not loaded.
+    //
+    // - This assumes that the title of most pages on the web will be set and hence when not reachable, the browser
+    //   will set title to the url/host (both chrome and brave) and 'problem loading page' for firefox.
+    // - There is probability that this might be true in case the <title> tag is omitted, but worst case it only reloads
+    //   those pages.
+    // - The benefit we get from this approach is the static nature of just observing the tabs in their current state.
+    //   which reduces the overhead of injecting content scripts to track urls that were loaded after the connection
+    //   was offline, it may also need extra permissions to inject code on error pages.
+    return url.startsWith(this.customGatewayUrl) &&
+      (url.includes(title) || title.toLowerCase() === 'problem loading page')
   }
 
   message ({ url }) {
