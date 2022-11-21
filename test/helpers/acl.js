9184147e8b55b74aede33f76fc56da617815dff9
Marcin Rataj <lidel@lidel.org>
2022-11-21 19:18:38 +0100

    chore: remove window.ipfs ACL tests

--- test/helpers/acl.js
+++
@@ -1,3 +0,0 @@
-const pairs = (obj) => Object.keys(obj).map(k => [k, obj[k]])
-// Converts an ACL expressed as a plain old object into an ACL Map
-export const objToAcl = (obj) => new Map(pairs(obj).map(e => [e[0], new Map(pairs(e[1]))]))
