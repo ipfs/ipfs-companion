b7df38ab86254999814e0ff6a5f6f210d950ea7b
galargh <piotr.galar@gmail.com>
2023-01-17 10:38:01 +0100

    fix: download-release-artifacts.sh

--- ci/download-release-artifacts.sh
+++ ci/download-release-artifacts.sh
@@ -6,7 +6,7 @@ IPFS_COMPANION_VERSION=${IPFS_COMPANION_VERSION:-$(jq -r '.version' ./add-on/man
 id="$(curl --retry 5 --no-progress-meter "https://api.github.com/repos/ipfs/ipfs-companion/releases/tags/v$IPFS_COMPANION_VERSION" | jq '.id')"
 assets="$(curl --retry 5 --no-progress-meter --location "https://api.github.com/repos/ipfs/ipfs-companion/releases/$id/assets" | jq -r '.[].name')"
 
-if [[ ! -d build ]]; then
+if [ ! -d build ]; then
   mkdir build
 fi
 
