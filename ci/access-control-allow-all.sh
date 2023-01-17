4bf55b68ed49e6b9f4bb6be92b2b856d4e3941d4
galargh <piotr.galar@gmail.com>
2023-01-17 10:10:36 +0100

    chore: allow overriding access control settings through env

--- ci/access-control-allow-all.sh
+++ ci/access-control-allow-all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
 
-ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
-ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["*"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin "${ACCESS_CONTROL_ALLOW_ORIGIN:-[\"*\"]}"
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods "${ACCESS_CONTROL_ALLOW_METHODS:-[\"*\"]}"
