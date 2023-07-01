4cbc14f70662b83f2af044330b1ff4df53250832
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2023-07-01 03:03:14 -0600

    fix(mv3): Enabling debugs based on default values.
    Signed-off-by: Nishant Arora <1895906+whizzzkid@users.noreply.github.com>

--- add-on/src/background/background.js
+++ add-on/src/background/background.js
@@ -1,10 +1,12 @@
 'use strict'
 /* eslint-env browser, webextensions */
 
+import debug from 'debug'
 import browser from 'webextension-polyfill'
 import createIpfsCompanion from '../lib/ipfs-companion.js'
 import { onInstalled } from '../lib/on-installed.js'
 import { getUninstallURL } from '../lib/on-uninstalled.js'
+import { optionDefaults } from '../lib/options.js'
 
 // register lifecycle hooks early, otherwise we miss first install event
 browser.runtime.onInstalled.addListener(onInstalled)
@@ -12,6 +14,8 @@ browser.runtime.setUninstallURL(getUninstallURL(browser))
 
 const init = async () => {
   await createIpfsCompanion()
+  const debugNs = (await browser.storage.local.get({ logNamespaces: optionDefaults.logNamespaces })).logNamespaces
+  debug.enable(debugNs)
 }
 
 init()
