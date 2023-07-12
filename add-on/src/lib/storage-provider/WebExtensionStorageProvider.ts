f3305fe21b81e2233eab595bb4d528de86e18946
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2023-07-12 04:00:46 -0600

    feat(mv3): :sparkles: Implementing Custom Async Store.
    Signed-off-by: Nishant Arora <1895906+whizzzkid@users.noreply.github.com>

---
+++ add-on/src/lib/storage-provider/WebExtensionStorageProvider.ts
@@ -0,0 +1,40 @@
+import type { consentTypes } from '@ipfs-shipyard/ignite-metrics/typings/countly'
+import type { StorageProviderInterface } from '@ipfs-shipyard/ignite-metrics/StorageProvider'
+import browser from 'webextension-polyfill'
+
+export class WebExtensionStorageProvider implements StorageProviderInterface {
+  async setStore (consentArray: consentTypes[]): Promise<void> {
+    try {
+      const jsonString = JSON.stringify(consentArray)
+      if ('localStorage' in globalThis) {
+        globalThis.localStorage.setItem('@ipfs-shipyard/ignite-metrics:consent', jsonString)
+      } else {
+        await browser.storage.local.set({ '@ipfs-shipyard/ignite-metrics:consent': jsonString })
+      }
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error(err)
+    }
+  }
+
+  async getStore (): Promise<consentTypes[]> {
+    try {
+      let jsonString;
+      if ('localStorage' in globalThis) {
+        jsonString = globalThis.localStorage.getItem('@ipfs-shipyard/ignite-metrics:consent')
+      } else {
+        jsonString = (await browser.storage.local.get(['@ipfs-shipyard/ignite-metrics:consent']))['@ipfs-shipyard/ignite-metrics:consent']
+      }
+      if (jsonString != null) {
+        return JSON.parse(jsonString)
+      }
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error(err)
+    }
+    /**
+     * Return minimal consent if there is nothing in the store.
+     */
+    return ['minimal']
+  }
+}
