ba631ea1fb69ad1e4fae02b29ba63f0f66406a4a
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-07-25 09:58:45 -0700

    test: merge mocha-setup files

--- test/setup/mocha-setup.js
+++ test/setup/mocha-setup.js
@@ -1,11 +1,33 @@
-import browser from 'sinon-chrome'
 import AbortController from 'abort-controller'
-browser.runtime.id = 'testid'
-global.browser = browser
-global.AbortController = AbortController
-global.chrome = browser
-global.navigator = {
-  clipboard: {
-    writeText: () => {}
+import { afterEach } from 'mocha'
+import sinon from 'sinon'
+import browser from 'sinon-chrome'
+import DeclarativeNetRequestMock from '../functional/lib/redirect-handler/declarativeNetRequest.mock.js'
+
+if (process.env.TEST_MV3 !== 'true') {
+  browser.runtime.id = 'testid'
+  global.browser = browser
+  global.AbortController = AbortController
+  global.chrome = browser
+  global.navigator = {
+    clipboard: {
+      writeText: () => {}
+    }
   }
+} else {
+  browser.runtime.id = 'testid'
+  global.browser = browser
+  global.AbortController = AbortController
+  global.chrome = browser
+  global.navigator = {
+    clipboard: {
+      writeText: () => {}
+    }
+  }
+  const sinonSandbox = sinon.createSandbox()
+  global.browser.declarativeNetRequest = sinonSandbox.spy(new DeclarativeNetRequestMock())
+
+  afterEach(function () {
+    sinonSandbox.resetHistory()
+  })
 }
