6e3c24e7dba03ebc4cf5352e9bf811cf271eeb5d
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-07-25 10:07:49 -0700

    test(setup): remove duplicate setup for mv3/mv2

--- test/setup/mocha-setup.js
+++ test/setup/mocha-setup.js
@@ -5,16 +5,17 @@ import browser from 'sinon-chrome'
 import DeclarativeNetRequestMock from '../functional/lib/redirect-handler/declarativeNetRequest.mock.js'
 import isMv3TestingEnabled from '../helpers/is-mv3-testing-enabled.js'
 
-if (isMv3TestingEnabled()) {
-  browser.runtime.id = 'testid'
-  global.browser = browser
-  global.AbortController = AbortController
-  global.chrome = browser
-  global.navigator = {
-    clipboard: {
-      writeText: () => {}
-    }
+browser.runtime.id = 'testid'
+global.browser = browser
+global.AbortController = AbortController
+global.chrome = browser
+global.navigator = {
+  clipboard: {
+    writeText: () => {}
   }
+}
+
+if (isMv3TestingEnabled()) {
   const sinonSandbox = sinon.createSandbox()
   global.browser.declarativeNetRequest = sinonSandbox.spy(new DeclarativeNetRequestMock())
 
@@ -22,13 +23,5 @@ if (isMv3TestingEnabled()) {
     sinonSandbox.resetHistory()
   })
 } else {
-  browser.runtime.id = 'testid'
-  global.browser = browser
-  global.AbortController = AbortController
-  global.chrome = browser
-  global.navigator = {
-    clipboard: {
-      writeText: () => {}
-    }
-  }
+
 }
