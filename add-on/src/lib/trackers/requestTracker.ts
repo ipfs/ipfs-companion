51c7dc6562773f9d1a34bfdce924ab4fe8eb612b
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2023-07-28 00:39:02 -0600

    fix(telemetry): :recycle: Refactor Request Tracker
    Signed-off-by: Nishant Arora <1895906+whizzzkid@users.noreply.github.com>

--- add-on/src/lib/trackers/requestTracker.ts
+++ add-on/src/lib/trackers/requestTracker.ts
@@ -1,40 +1,34 @@
+import debug from 'debug'
 import { trackEvent } from '../telemetry.js'
-import { IMessageHandler } from './IMessageHandler.js'
 
-interface IRequestTrackerMsg {
-  type: string
-  requestType: string
-}
-
-export default class RequestTracker implements IMessageHandler<IRequestTrackerMsg> {
+export class RequestTracker {
   private readonly eventKey: 'url-observed' | 'url-resolved'
   private readonly flushInterval: number = 1000 * 60 * 5 // 5 minutes
-  private readonly msgKey: string = 'ipfs-companion:track-request:'
+  private readonly log: debug.Debugger & { error?: debug.Debugger }
   private lastSync: number = Date.now()
   private requestTypeStore: Record<string, number> = {}
 
   constructor (eventKey: 'url-observed' | 'url-resolved') {
     this.eventKey = eventKey
+    this.log = debug(`ipfs-companion:request-tracker:${eventKey}`)
+    this.log.error = debug(`ipfs-companion:request-tracker:${eventKey}:error`)
     this.setupFlushScheduler()
   }
 
-  get checkKey (): string {
-    return this.msgKey + this.eventKey
-  }
-
-  check ({ type }: IRequestTrackerMsg): boolean {
-    return type === this.checkKey
-  }
-
-  async handler ({ requestType }: IRequestTrackerMsg): Promise<any> {
-    if (!(requestType in this.requestTypeStore)) {
-      this.requestTypeStore[requestType] = 0
+  async track ({ type }: { type: string }): Promise<any> {
+    this.log(`track ${type}`, JSON.stringify(this.requestTypeStore))
+    if (!(type in this.requestTypeStore)) {
+      this.requestTypeStore[type] = 0
     }
-    this.requestTypeStore[requestType] += 1
+    this.requestTypeStore[type] += 1
   }
 
   private flushStore (): void {
     const count = Object.values(this.requestTypeStore).reduce((a, b): number => a + b, 0)
+    if (count === 0) {
+      this.log('nothing to flush')
+      return
+    }
     trackEvent({
       key: this.eventKey,
       count,
