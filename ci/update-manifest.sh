00b4e2bf5d2c9080493bfc882d3881a84bb42ad1
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2023-09-13 02:00:06 -0600

    fix: :pencil2: Beta naming
    Signed-off-by: Nishant Arora <1895906+whizzzkid@users.noreply.github.com>

--- ci/update-manifest.sh
+++ ci/update-manifest.sh
@@ -23,7 +23,7 @@ function set-manifest {
 # Name includes git revision to make QA and bug reporting easier for users :-)
 REVISION=$(git show-ref --head HEAD | head -c 7)
 if [ "$RELEASE_CHANNEL" = "beta" ]; then
-    set-manifest ".name = \"IPFS Companion (@ $REVISION)\""
+    set-manifest ".name = \"IPFS Companion (Beta @ $REVISION)\""
 else
     set-manifest ".name = \"IPFS Companion (Dev Build @ $REVISION)\""
 fi
